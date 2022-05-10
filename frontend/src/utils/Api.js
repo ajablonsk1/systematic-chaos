1fe61fb042d94983b0400f2ad298c6b9af85c1de
kwdrt <jakubkcichocki@gmail.com>
2022-05-10 14:10:29 +0200

    SC-41: Added example activity map for the map screen and 'API' call to access it

--- frontend/src/utils/Api.js
+++ frontend/src/utils/Api.js
@@ -1,10 +1,21 @@
 import expedition from '../storage/exampleExpeditionData.json';
+import mapData from '../storage/exampleActivityMap.json';
 import { START_GRAPH_NODE_ID } from './constants';
 
-export function getStartQuestions() {
+//for now we ignore IDs given for our mock
+
+export function getStartQuestions(chapterID, expeditionID) {
     let startNode = expedition.graph.find(x => x.id === START_GRAPH_NODE_ID);
     let startQuestions = startNode.next.map(nextNodeId =>
         expedition.graph.find(x => x.id === nextNodeId)
     );
     return startQuestions;
+}
+
+export function getActivityMap(chapterID) {
+    console.log(mapData);
+    console.log(mapData.activityMap);
+    console.log(mapData.activityMap.mapSizeX);
+    console.log(mapData.activityMap.mapSizeY);
+    return mapData;
 }
