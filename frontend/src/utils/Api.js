064f02e6325bc1c9e9139917a0b16741ea273587
kwdrt <jakubkcichocki@gmail.com>
2022-05-10 15:20:44 +0200

    SC-41: Added getNextQuestions

--- frontend/src/utils/Api.js
+++ frontend/src/utils/Api.js
@@ -2,7 +2,7 @@ import expedition from '../storage/exampleExpeditionData.json';
 import mapData from '../storage/exampleActivityMap.json';
 import { START_GRAPH_NODE_ID } from './constants';
 
-//for now we ignore IDs given for our mock
+//for now we ignore IDs given for our mock API calls
 
 export function getStartQuestions(chapterID, expeditionID) {
     let startNode = expedition.graph.find(x => x.id === START_GRAPH_NODE_ID);
@@ -12,10 +12,18 @@ export function getStartQuestions(chapterID, expeditionID) {
     return startQuestions;
 }
 
+//check whether currentNodeID is not END_GRAPH_NODE_ID before function call
+
+export function getNextQuestions(chapterID, expeditionID, currentNodeID) {
+    let currentNode = expedition.graph.find(x => x.id === currentNodeID);
+    let nextQuestions = currentNode.next.map(nextNodeId =>
+        expedition.graph.find(x => x.id === nextNodeId)
+    );
+    return nextQuestions;
+}
+
+//currently wrapped in "activityMap" since it will be probably an extracted part of chapter info
+
 export function getActivityMap(chapterID) {
-    console.log(mapData);
-    console.log(mapData.activityMap);
-    console.log(mapData.activityMap.mapSizeX);
-    console.log(mapData.activityMap.mapSizeY);
     return mapData;
 }
