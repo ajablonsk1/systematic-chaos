76d7b4ac6d52f3ea078c678026b26933f1f9eefe
dmncp <damiancyper@gmail.com>
2022-05-13 19:25:41 +0200

    SC-48: open question view and removing used items from localStorageafter expedition

---
+++ frontend/src/components/QuestionAndOptions/OpenQuestionPage/OpenQuestionStyle.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const UserAnswerArea = styled.textarea`
+    width: 100%;
+    margin-top: 10px;
+    padding: 20px;
+    font-size: 20px;
+    border-radius: 10px;
+    border: 3px solid var(--dark-blue);
+`;
