7304cce52638a41c890bcf25ea4ad725a398d71c
ajablonsk1 <adrian.tomasz.jablonski@gmail.com>
2022-05-13 14:03:39 +0200

    SC-35: Added sidebar for desktop and mobile view

--- frontend/src/components/Sidebar/Sidebar.js
+++ frontend/src/components/Sidebar/Sidebar.js
@@ -1,18 +1,27 @@
 import React from 'react';
-import {buildNavLink} from "./navBuilder";
-import {SidebarEdit} from "./SidebarStyles";
+import {buildNavLink, buildNavLinkMobile} from "./navBuilder";
+import {SidebarEdit, NavEdit, LogoDiv, MobileNavbar} from "./SidebarStyles";
 import {Container, Nav} from "react-bootstrap";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faFire } from '@fortawesome/free-solid-svg-icons'
 
 function Sidebar({link_titles}) {
     return (
+        <>
         <SidebarEdit variant="dark" className="d-md-block d-none">
             <Container className="d-flex flex-column">
-                <div id="logo"><p>Systematic Chaos</p></div>
-                <Nav className="me-auto d-flex flex-column" id="menu-options">
-                    {link_titles.map(([to, linkTitle]) => buildNavLink(to, linkTitle))}
-                </Nav>
+                <LogoDiv id="logo"><p><FontAwesomeIcon style={{marginRight: '10px'}} icon={faFire} />Systematic Chaos</p></LogoDiv>
+                <NavEdit className="me-auto d-flex flex-column" id="menu-options">
+                    {link_titles.map(([to, linkTitle]) => buildNavLink(to, linkTitle[0]))}
+                </NavEdit>
             </Container>
         </SidebarEdit>
+        <MobileNavbar className="d-md-none d-sm-block">
+            <Nav className="d-flex justify-content-center">
+                {link_titles.map(([to, linkTitle]) => buildNavLinkMobile(to, linkTitle[1]))}
+            </Nav>
+        </MobileNavbar>
+        </>
     );
 }
 
