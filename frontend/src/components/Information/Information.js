14b1b1c9271f23a1dc333f2495451de58cd9e51a
barto14753 <wlodarski_bartosz@o2.pl>
2022-05-31 23:28:01 +0200

    [SC-68] Added information component

--- frontend/src/components/Information/Information.js
+++ frontend/src/components/Information/Information.js
@@ -1,7 +1,50 @@
 import React from 'react';
+import { useLocation } from 'react-router-dom';
+import { getInformation } from '../../storage/information';
+import { getActivityImg, getActivityTypeName } from '../../utils/constants';
+import {
+    HeaderRow,
+    HeaderCol,
+    ActivityImg,
+    ActivityName,
+    ActivityType,
+    ActivityCol,
+    FullDivider,
+    SmallDivider,
+} from '../ActivityInfo/ActivityInfoStyles';
+import { InfoContainer } from '../ActivityInfo/InfoContainer';
+import { Content } from '../App/AppGeneralStyles';
+import { InformationImage } from './InformationStyles';
+
 
 export default function Information() {
+    const location = useLocation()
+    const { activityId: informationId } = location.state;
+    const information = getInformation(informationId);
+
     return (
-        <h1>Information</h1>
+        <Content>
+            <InfoContainer>
+                <ActivityCol>
+                    <HeaderCol>
+                    <HeaderRow>
+                        <ActivityImg src={getActivityImg(information.type)}></ActivityImg>
+                        <ActivityType>{getActivityTypeName(information.type)}</ActivityType>
+                        <ActivityName>{information.name}</ActivityName>
+                        </HeaderRow>
+                        <FullDivider />
+                    </HeaderCol>
+                    <div>
+                        {information.images.map((image, idx) => (
+                            <InformationImage src={image} alt={idx} key={idx}/>
+                        ))}
+                    </div>
+                    <SmallDivider />
+                    <div>
+                        <p>{information.content}</p>
+                    </div>
+                </ActivityCol>
+            </InfoContainer>
+        </Content>
     )
 }
