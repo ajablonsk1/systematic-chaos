65205356f9dd2bf02388afe1fbaa1879ca91d78b
kwdrt <56168780+kwdrt@users.noreply.github.com>
2022-06-09 15:41:57 +0200

    [SC-79] fixed route to survey (#36)
    * [SC-79] fixed route to survey
    
    * [SC-79] key fix

--- frontend/src/components/SurveyTask/IconColumn.js
+++ frontend/src/components/SurveyTask/IconColumn.js
@@ -4,8 +4,8 @@ import { Col } from 'react-bootstrap';
 export const IconColumn = props => {
     return (
         <Col>
-            {props.icons.map(faIcon => {
-                return faIcon && <FontAwesomeIcon icon={faIcon} />;
+            {props.icons.map((faIcon, idx) => {
+                return faIcon && <FontAwesomeIcon key={idx} icon={faIcon} />;
             })}
         </Col>
     );
