2244171a98595b852a15127efdde64faf1351cf2
kwdrt <jakubkcichocki@gmail.com>
2022-06-12 16:46:50 +0200

    [SC-53] rewrite to styled

---
+++ frontend/src/components/ActivityAssessmentDetails/ActivityAssesmentDetailsStyles.js
@@ -0,0 +1,139 @@
+import styled from 'styled-components';
+import { Col, Row, Button } from 'react-bootstrap';
+
+export const ContentCol = styled(Col)`
+    padding-top: 15px;
+    margin-left: auto;
+    margin-right: auto;
+`;
+
+export const ActivityTitle = styled.div`
+    width: 100%;
+    text-align: center;
+    padding-top: 10px;
+    padding-bottom: 5px;
+    background-color: var(--dark-blue);
+    color: var(--font-color);
+`;
+
+export const TopInfo = styled(Row)`
+    justify-content: space-around;
+    padding-top: 6px;
+    max-width: 1500px;
+    margin-left: auto;
+    margin-right: auto;
+    color: var(--font-color);
+`;
+
+export const TopInfoCard = styled.div`
+    background-color: var(--dark-blue);
+    width: 740px;
+    margin-top: 5px;
+    padding: 10px;
+`;
+
+export const UserInfo = styled.div`
+    display: flex;
+    justify-content: space-between;
+    flex-flow: column;
+    padding-left: 10px;
+`;
+
+export const ActivityInfo = styled.p`
+    font-size: 0.75rem;
+    max-height: 120px;
+    overflow-y: auto;
+`;
+
+export const AnswerRow = styled(Row)`
+    width: 100%;
+    min-height: 300px;
+    max-height: 500px;
+    background-color: var(--dark-blue);
+    margin-left: auto;
+    margin-right: auto;
+    margin-top: 10px;
+`;
+
+export const AnswerContent = styled.p`
+    font-size: 1rem;
+    max-height: 120px;
+    overflow-y: auto;
+`;
+
+export const AnswerCol = styled(Col)`
+    margin-top: 20px;
+    color: var(--font-color);
+`;
+
+export const RemarksCol = styled(Col)`
+    width: 100%;
+    min-height: 150px;
+    max-height: 300px;
+    background-color: var(--dark-blue);
+    margin-left: auto;
+    margin-right: auto;
+    margin-top: 10px;
+    color: var(--font-color);
+    padding-bottom: 5px;
+`;
+
+export const PointsRow = styled(Row)`
+    align-items: center;
+    display: flex;
+    justify-content: space-around;
+    background-color: var(--dark-blue);
+    width: 30%;
+    margin-left: auto;
+    margin-right: auto;
+    margin-top: 10px;
+    text-align: center;
+    color: var(--font-color);
+    padding: 10px;
+`;
+
+export const PointsInput = styled.input`
+    width: 50px;
+    color: var(--font-color);
+    border: 1px solid var(--font-color);
+    background-color: var(--dark-blue);
+`;
+
+export const PointsMax = styled.p`
+    top: 50%;
+    position: relative;
+    margin: 0;
+    margin-left: 5px;
+`;
+
+export const AcceptButton = styled(Button)`
+    margin-left: 50%;
+    transform: translateX(-50%);
+    margin-top: 15px;
+    background-color: var(--button-green);
+    border-color: var(--button-green);
+`;
+
+export const RemarksTextArea = styled.textarea`
+    width: 100%;
+    padding: 3px;
+    border: none;
+    max-height: 220px;
+    margin-left: auto;
+    margin-right: auto;
+    color: var(--font-color);
+    border: 1px solid var(--font-color);
+    background-color: var(--dark-blue);
+`;
+
+export const RemainingCount = styled.p`
+    background-color: var(--dark-blue);
+    color: var(--font-color);
+    padding: 10px;
+    width: 35%;
+    margin-bottom: 5px;
+    margin-top: 20px;
+    text-align: center;
+    position: relative;
+    margin-left: 65%;
+`;
--- frontend/src/components/ActivityAssessmentDetails/ActivityAssessmentDetails.js
+++ frontend/src/components/ActivityAssessmentDetails/ActivityAssessmentDetails.js
@@ -2,6 +2,24 @@ import { Content } from '../App/AppGeneralStyles';
 import { Button, Col, Row } from 'react-bootstrap';
 import { FullDivider } from '../ActivityInfo/ActivityInfoStyles';
 import { useNavigate, useLocation } from 'react-router-dom';
+import {
+    ContentCol,
+    ActivityTitle,
+    TopInfo,
+    TopInfoCard,
+    UserInfo,
+    AnswerRow,
+    AnswerContent,
+    ActivityInfo,
+    AnswerCol,
+    RemarksCol,
+    PointsInput,
+    PointsMax,
+    AcceptButton,
+    RemarksTextArea,
+    PointsRow,
+    RemainingCount,
+} from './ActivityAssesmentDetailsStyles';
 
 //https://www.flaticon.com/free-icon/user-picture_21104
 import userPicture from '../../utils/resources/user-picture.png';
@@ -9,99 +27,44 @@ import userPicture from '../../utils/resources/user-picture.png';
 export default function ActivityAssessmentDetails() {
     const navigate = useNavigate();
     const location = useLocation();
-    console.log(location);
+    //console.log(location);
 
     //we will get data based on the id
     const { activityId: activityId } = location.state;
 
     return (
         <Content>
-            <Col
-                style={{
-                    paddingTop: '15px',
-                    marginLeft: 'auto',
-                    marginRight: 'auto',
-                }}
-            >
-                <div
-                    style={{
-                        width: '100%',
-                        textAlign: 'center',
-                        paddingTop: '10px',
-                        paddingBottom: '5px',
-                        backgroundColor: 'var(--dark-blue)',
-                        color: 'var(--font-color)',
-                    }}
-                >
+            <ContentCol>
+                <ActivityTitle>
                     <h4>Zadanie bojowe - SuperSystem I</h4>
-                </div>
-                <Row
-                    style={{
-                        justifyContent: 'space-around',
-                        paddingTop: '6px',
-                        maxWidth: '1500px',
-                        marginLeft: 'auto',
-                        marginRight: 'auto',
-                        color: 'var(--font-color)',
-                    }}
-                >
-                    <div
-                        style={{
-                            backgroundColor: 'var(--dark-blue)',
-                            width: '740px',
-                            marginTop: '5px',
-                            padding: '10px',
-                        }}
-                    >
+                </ActivityTitle>
+                <TopInfo>
+                    <TopInfoCard>
                         <h4 style={{ textAlign: 'center' }}>Informacje o użytkowniku</h4>
                         <FullDivider />
                         <Row styles={{ justifyContent: 'center' }}>
                             <img src={userPicture} style={{ paddingLeft: '20px' }}></img>
-                            <div
-                                style={{
-                                    display: 'flex',
-                                    justifyContent: 'space-between',
-                                    flexFlow: 'column',
-                                    paddingLeft: '10px',
-                                }}
-                            >
+                            <UserInfo>
                                 <h5 styles={{ width: '100%' }}>Użytkownik testowy</h5>
                                 <h5 styles={{ width: '100%' }}>zadanie oddane w terminie</h5>
-                            </div>
+                            </UserInfo>
                         </Row>
-                    </div>
-                    <div
-                        style={{
-                            backgroundColor: 'var(--dark-blue)',
-                            width: '740px',
-                            marginTop: '5px',
-                            padding: '10px',
-                        }}
-                    >
+                    </TopInfoCard>
+                    <TopInfoCard>
                         <h4 style={{ textAlign: 'center' }}>Informacje o aktywności</h4>
                         <FullDivider />
                         <p style={{ marginBottom: '1px' }}>Treść:</p>
-                        <p style={{ fontSize: '0.75rem', maxHeight: '120px', overflowY: 'auto' }}>
+                        <ActivityInfo>
                             Narysuj przykład sieci składającej się z 3 routerów, 5 komputerów, 4
                             switchy wraz z wypisanym rodzajem użytych kabli na każdym połączeniu.
-                        </p>
+                        </ActivityInfo>
                         <p style={{ textAlign: 'center' }}>Punkty: 30</p>
-                    </div>
-                </Row>
-                <Row
-                    style={{
-                        width: '100%',
-                        minHeight: '300px',
-                        maxHeight: '500px',
-                        backgroundColor: 'var(--dark-blue)',
-                        marginLeft: 'auto',
-                        marginRight: 'auto',
-                        marginTop: '10px',
-                    }}
-                >
-                    <Col style={{ marginTop: '20px', color: 'var(--font-color)' }}>
+                    </TopInfoCard>
+                </TopInfo>
+                <AnswerRow>
+                    <AnswerCol>
                         <h4>Odpowiedź</h4>
-                        <p style={{ fontSize: '1rem', maxHeight: '120px', overflowY: 'auto' }}>
+                        <AnswerContent>
                             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
                             ullamcorper finibus magna, a pellentesque eros consequat id. Donec vitae
                             tristique purus, quis rutrum felis. Nam ultricies, risus at pharetra
@@ -113,110 +76,26 @@ export default function ActivityAssessmentDetails() {
                             sapien. Curabitur blandit imperdiet suscipit. Phasellus placerat libero
                             sed tristique pulvinar. Vivamus bibendum augue urna, accumsan interdum
                             risus venenatis quis.
-                        </p>
-                    </Col>
-                </Row>
+                        </AnswerContent>
+                    </AnswerCol>
+                </AnswerRow>
 
-                <Col
-                    style={{
-                        width: '100%',
-                        minHeight: '150px',
-                        maxHeight: '300px',
-                        backgroundColor: 'var(--dark-blue)',
-                        marginLeft: 'auto',
-                        marginRight: 'auto',
-                        marginTop: '10px',
-                        color: 'var(--font-color)',
-                        paddingBottom: '5px',
-                    }}
-                >
+                <RemarksCol>
                     <h4>Uwagi:</h4>
-                    <textarea
-                        style={{
-                            width: '100%',
-                            padding: '3px',
-                            border: 'none',
-                            maxHeight: '220px',
-                            marginLeft: 'auto',
-                            marginRight: 'auto',
-                            color: 'var(--font-color)',
-                            border: '1px solid var(--font-color)',
-                            backgroundColor: 'var(--dark-blue)',
-                        }}
-                    ></textarea>
-                </Col>
+                    <RemarksTextArea />
+                </RemarksCol>
 
-                <Row
-                    style={{
-                        alignItems: 'center',
-                        display: 'flex',
-                        justifyContent: 'space-around',
-                        backgroundColor: 'var(--dark-blue)',
-                        width: '30%',
-                        marginLeft: 'auto',
-                        marginRight: 'auto',
-                        marginTop: '10px',
-                        textAlign: 'center',
-                        color: 'var(--font-color)',
-                        padding: '10px',
-                    }}
-                >
+                <PointsRow>
                     <p style={{ top: '50%', position: 'relative', margin: '0' }}>Punkty: </p>
                     <Row style={{ display: 'flex' }}>
-                        <input
-                            style={{
-                                width: '50px',
-                                color: 'var(--font-color)',
-                                border: '1px solid var(--font-color)',
-                                backgroundColor: 'var(--dark-blue)',
-                            }}
-                            type="number"
-                            min={0}
-                            max={20}
-                        ></input>
-                        <p
-                            style={{
-                                top: '50%',
-                                position: 'relative',
-                                margin: '0',
-                                marginLeft: '5px',
-                            }}
-                        >
-                            {' '}
-                            / 20
-                        </p>
+                        <PointsInput type="number" min={0} max={20}></PointsInput>
+                        <PointsMax>/ 20</PointsMax>
                     </Row>
-                </Row>
+                </PointsRow>
 
-                <Button
-                    style={{
-                        marginLeft: '50%',
-                        transform: 'translateX(-50%)',
-                        marginTop: '15px',
-                        backgroundColor: 'var(--button-green)',
-                        borderColor: 'var(--button-green)',
-                    }}
-                >
-                    Zaakceptuj i przejdź do kolejnej odpowiedzi
-                </Button>
-
-                <p
-                    style={{
-                        backgroundColor: 'var(--dark-blue)',
-                        color: 'var(--font-color)',
-                        padding: '10px',
-                        width: '35%',
-                        marginBottom: '5px',
-                        marginTop: '20px',
-                        right: 0,
-                        textAlign: 'center',
-                        position: 'relative',
-                        transform: 'translateX(-100%)',
-                    }}
-                >
-                    Pozostało 29 odpowiedzi do sprawdzenia
-                </p>
-            </Col>
+                <AcceptButton>Zaakceptuj i przejdź do kolejnej odpowiedzi</AcceptButton>
+                <RemainingCount>Pozostało 29 odpowiedzi do sprawdzenia</RemainingCount>
+            </ContentCol>
         </Content>
     );
 }
