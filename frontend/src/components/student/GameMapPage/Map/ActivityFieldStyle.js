a9866e7663f4f11b81708abd9df9a11ede97b179
Damian Cyper <damiancyper@gmail.com>
2022-07-04 21:13:26 +0200

    [SC-89] game map displaying bug fixed

--- frontend/src/components/student/GameMapPage/Map/ActivityFieldStyle.js
+++ frontend/src/components/student/GameMapPage/Map/ActivityFieldStyle.js
@@ -4,18 +4,12 @@ import {Col} from "react-bootstrap";
 export const ActivityCol = styled(Col)`
     border: 2px black solid;
     border-radius: 5px;
-    width: auto;
-    height: auto;
     margin: 1px;
     padding: 0;
+    flex-basis: auto;
+    flex-grow: 0;
 
-    & img {
-        width: 100%;
-        height: 100%;
-        border-radius: 5px;
-    }
-
-    & div {
+    & img, & div {
         width: 100%;
         height: 100%;
         border-radius: 5px;
