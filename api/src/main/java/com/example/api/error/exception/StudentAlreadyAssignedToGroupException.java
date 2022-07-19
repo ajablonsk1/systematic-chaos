0b47e1bcd340bb1becf09383953e7fa9022c6771
barto14753 <wlodarski_bartosz@o2.pl>
2022-07-19 22:03:18 +0200

    [SC-99] Added set student group endpoint

---
+++ api/src/main/java/com/example/api/error/exception/StudentAlreadyAssignedToGroupException.java
@@ -0,0 +1,21 @@
+package com.example.api.error.exception;
+
+import com.example.api.model.group.Group;
+import com.example.api.model.user.User;
+
+public class StudentAlreadyAssignedToGroupException extends Exception{
+    User student;
+    Group group;
+
+    public StudentAlreadyAssignedToGroupException(String message, User student, Group group){
+        super(message);
+        this.student = student;
+        this.group = group;
+    }
+
+    @Override
+    public String getLocalizedMessage() {
+        return "Student " + student.getFirstName()  + " " +
+                student.getLastName() + " is already assigned to this group " + group.getName();
+    }
+}
