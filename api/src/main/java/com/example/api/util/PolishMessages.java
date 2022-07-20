b4b5fe3290639beaefb200dce79c73481b63ec54
Adrian Jabłoński <73038429+ajablonsk1@users.noreply.github.com>
2022-07-20 21:36:54 +0200

    [SC-124] Refactored saveGroup() and changed a bit registerUser(). Now both classes throw exceptions with message in polish (#69)

---
+++ api/src/main/java/com/example/api/util/PolishMessages.java
@@ -0,0 +1,8 @@
+package com.example.api.util;
+
+public class PolishMessages {
+    public static final String EMAIL_TAKEN = "Podany adres email jest zajęty";
+    public static final String INDEX_TAKEN = "Podany numer indeksu jest zajęty";
+    public static final String GROUP_NAME_TAKEN = "Podana nazwa grupy jest zajęta";
+    public static final String GROUP_CODE_TAKEN = "Podany kod grupy jest zajęty";
+}
