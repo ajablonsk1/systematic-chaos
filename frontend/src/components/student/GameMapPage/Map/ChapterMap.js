7684744ca64c028d5fe3daa26d536da704a37975
Damian Cyper <damiancyper@gmail.com>
2022-07-23 16:10:45 +0200

    [SC-126] chapter management view, edition modal, new ActivityField size generator, FormCol refactor

--- frontend/src/components/student/GameMapPage/Map/ChapterMap.js
+++ frontend/src/components/student/GameMapPage/Map/ChapterMap.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect, useCallback } from 'react'
 import { Row } from 'react-bootstrap'
 import Loader from '../../../general/Loader/Loader'
 import { Map } from '../GameMapStyles'
@@ -20,8 +20,34 @@ function createMap(map) {
   return rowsObj
 }
 
-export default function ChapterMap({ map }) {
+export default function ChapterMap({ map, marginNeeded, parentRef, mapClickable }) {
+  const [size, setSize] = useState(undefined)
+
   const rows = createMap(map)
+
+  const getParentSize = useCallback(() => {
+    const emptySpaceX = 32 + rows[0].length * 2
+    const emptySpaceY = 32 + rows.length * 2 + marginNeeded ? 32 : 0 // margin-x only
+    console.log(parentRef?.current?.offsetWidth, parentRef.current?.offsetHeight)
+    if (parentRef?.current?.offsetWidth && parentRef.current?.offsetHeight) {
+      return {
+        x: parentRef?.current?.offsetWidth - emptySpaceX,
+        y: parentRef.current?.offsetHeight - emptySpaceY
+      }
+    } else return { x: 0, y: 0 }
+  }, [marginNeeded, parentRef, rows])
+
+  useEffect(() => {
+    function setHeight() {
+      let { x, y } = getParentSize()
+      console.log(x, y)
+      const possibleSize = Math.floor(Math.min(x / map.mapSizeX, y / map.mapSizeY))
+      setSize(possibleSize ?? 0)
+    }
+
+    setHeight() // first time, on component mount
+    window.addEventListener('resize', setHeight) // always when window resize
+  }, [parentRef, map.mapSizeX, map.mapSizeY, getParentSize])
 
   const MapRows = rows.map((row, idx1) => (
     <Row key={idx1} className='mx-auto'>
@@ -31,8 +57,8 @@ export default function ChapterMap({ map }) {
           activity={activity}
           posX={idx2}
           posY={idx1}
-          mapSizeY={map.mapSizeY}
-          mapSizeX={map.mapSizeX}
+          colClickable={mapClickable}
+          colSize={size}
         />
       ))}
     </Row>
@@ -43,7 +69,7 @@ export default function ChapterMap({ map }) {
       {!rows ? (
         <Loader />
       ) : (
-        <Map fluid className='h-100 my-5'>
+        <Map fluid className={`${marginNeeded && 'my-2'} h-100`}>
           {MapRows}
         </Map>
       )}
