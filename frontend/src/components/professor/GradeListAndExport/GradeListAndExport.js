182b2b908c14d715d67f6685e8b27f1d5f6251fd
Damian Cyper <63460537+dmncp@users.noreply.github.com>
2022-07-26 19:32:14 +0200

    [SC-110] change student group mechanism, replace mock data into db data (#76)

--- frontend/src/components/professor/GradeListAndExport/GradeListAndExport.js
+++ frontend/src/components/professor/GradeListAndExport/GradeListAndExport.js
@@ -1,25 +1,35 @@
 import { Tab } from 'react-bootstrap'
 import UsersTable from './UsersTable'
-import { getAllParticipants, getGroups } from '../ParticipantsPage/mockData'
+// import { getAllParticipants, getGroups } from '../ParticipantsPage/mockData'
 import { GradesContent } from './GradeListAndExportStyles'
 import { TabsContainer } from './GradeListAndExportStyles'
+import { useEffect, useState } from 'react'
+import GroupService from '../../../services/group.service'
+import Loader from '../../general/Loader/Loader'
 
 export default function GradeListAndExport() {
-  const allUsers = getAllParticipants()
-  const groups = getGroups()
+  const [allGroups, setAllGroups] = useState(undefined)
+
+  useEffect(() => {
+    GroupService.getGroups().then((response) => setAllGroups(response))
+  }, [])
 
   return (
     <GradesContent>
       <TabsContainer>
-        <Tab eventKey={'all'} title={'Wszyscy'}>
-          <UsersTable users={allUsers} />
+        <Tab eventKey={'wszyscy'} title={'Wszyscy'}>
+          <UsersTable />
         </Tab>
 
-        {groups.map((group, index) => (
-          <Tab key={index + group.groupKey} title={group.groupName} eventKey={group.groupKey}>
-            <UsersTable users={group} />
-          </Tab>
-        ))}
+        {allGroups ? (
+          allGroups.map((group, index) => (
+            <Tab key={index + group.name} title={group.name} eventKey={group.name}>
+              <UsersTable groupId={group.id} groupName={group.name} />
+            </Tab>
+          ))
+        ) : (
+          <Loader />
+        )}
       </TabsContainer>
     </GradesContent>
   )
--- frontend/src/components/professor/ParticipantsPage/ChangeGroupModal.js
+++ frontend/src/components/professor/ParticipantsPage/ChangeGroupModal.js
@@ -1,42 +1,98 @@
-import React from 'react'
-import { Button, Card, Form, Modal } from 'react-bootstrap'
+import React, { useEffect, useState } from 'react'
+import { Button, Card, Form, Modal, ModalBody, ModalFooter, Spinner } from 'react-bootstrap'
 import CardHeader from 'react-bootstrap/CardHeader'
-import { getGroups } from './mockData'
+import Loader from '../../general/Loader/Loader'
+import GroupService from '../../../services/group.service'
 
 function ChangeGroupModal(props) {
-  const studentData = props.data
-  const groups = getGroups()
+  const [student, setStudent] = useState()
+  const [groups, setGroups] = useState([])
+  const [newGroup, setNewGroup] = useState()
+  const [isFetching, setIsFetching] = useState(false)
+  const [error, setError] = useState()
+  const [successModalOpen, setSuccessModalOpen] = useState(false)
+
+  useEffect(() => setStudent(props.student), [props.student])
+  useEffect(() => {
+    GroupService.getGroups().then((response) => {
+      setGroups(response)
+      setNewGroup(response.find((group) => group.name === student?.groupName)) //defaultValue in select list
+    })
+  }, [student])
+
+  const selectGroup = (event) => {
+    setNewGroup(groups.find((group) => group.name === event.target.value))
+  }
+
+  const submitChange = () => {
+    setIsFetching(true)
+    GroupService.changeStudentGroup(student?.id, newGroup?.id)
+      .then(() => {
+        setIsFetching(false)
+        props.setModalOpen(false)
+        setSuccessModalOpen(true)
+      })
+      .catch((error) => {
+        setError(error)
+        setIsFetching(false)
+      })
+  }
 
   return (
-    <Modal show={props.show}>
-      <Card>
-        <CardHeader className={'text-center'}>
-          <Card.Title>Zmiana grupy</Card.Title>
-          <p>Zmień grupę wybranego studenta wybierając jego nową grupę z poniższej listy</p>
-        </CardHeader>
-        <Card.Body>
-          <h6>Wybrany student: {studentData?.name}</h6>
-          <Form>
-            <span>Przypisz do grupy: </span>
-            <select defaultValue={studentData?.groupName}>
-              {groups.map((group, index) => (
-                <option key={group.groupKey + index} value={group.groupName}>
-                  {group.groupName}
-                </option>
-              ))}
-            </select>
-          </Form>
-        </Card.Body>
-        <Card.Footer className={'d-flex justify-content-center align-items-center'}>
-          <Button onClick={() => props.setModalOpen(false)} variant={'danger'}>
-            Anuluj
+    <>
+      <Modal show={props.show}>
+        <Card>
+          <CardHeader className={'text-center'}>
+            <Card.Title>Zmiana grupy</Card.Title>
+            <p>Zmień grupę wybranego studenta wybierając jego nową grupę z poniższej listy</p>
+          </CardHeader>
+          {!student || !groups ? (
+            <Loader />
+          ) : (
+            <>
+              <Card.Body>
+                <h6>
+                  Wybrany student: {student?.firstName} {student?.lastName}
+                </h6>
+                <Form>
+                  <span>Przypisz do grupy: </span>
+                  <select defaultValue={student?.groupName} onChange={selectGroup}>
+                    {groups.map((group) => (
+                      <option key={group.id} value={group.name}>
+                        {group.name}
+                      </option>
+                    ))}
+                  </select>
+                </Form>{' '}
+                {error && <div className={'mt-3 text-center text-danger w-100'}>{error}</div>}
+              </Card.Body>
+              <Card.Footer className={'d-flex justify-content-center align-items-center'}>
+                <Button onClick={() => props.setModalOpen(false)} variant={'danger'}>
+                  Anuluj
+                </Button>
+                <Button style={{ backgroundColor: 'var(--button-green)' }} className={'ml-2'} onClick={submitChange}>
+                  {isFetching ? <Spinner animation={'border'} size={'sm'} /> : <span>Zapisz</span>}
+                </Button>
+              </Card.Footer>
+            </>
+          )}
+        </Card>
+      </Modal>
+      <Modal show={successModalOpen}>
+        <Modal.Header>
+          <Modal.Title>Zmiana zakończona.</Modal.Title>
+        </Modal.Header>
+        <ModalBody>
+          Grupa studenta {student?.firstName} {student?.lastName}
+          została zmieniona pomyślnie z {student?.groupName} na {newGroup?.name}
+        </ModalBody>
+        <ModalFooter>
+          <Button variant={'success'} onClick={() => setSuccessModalOpen(false)}>
+            Zakończ
           </Button>
-          <Button style={{ backgroundColor: 'var(--button-green)' }} className={'ml-2'}>
-            Zapisz
-          </Button>
-        </Card.Footer>
-      </Card>
-    </Modal>
+        </ModalFooter>
+      </Modal>
+    </>
   )
 }
 
