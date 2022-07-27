17c9aed57515ef3c09201609185082d923a14664
Adrian Jabłoński <73038429+ajablonsk1@users.noreply.github.com>
2022-07-27 20:00:22 +0200

    [SC-133] Added NoArgsContructor for every dto request (#81)

--- api/src/main/java/com/example/api/dto/request/activity/task/SaveFileToFileTaskResultForm.java
+++ api/src/main/java/com/example/api/dto/request/activity/task/SaveFileToFileTaskResultForm.java
@@ -3,9 +3,11 @@ package com.example.api.dto.request.activity.task;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class SaveFileToFileTaskResultForm {
     @Schema(required = true) private Long fileTaskId;
     @Schema(required = true) private String studentEmail;
