9f3ceae82844cc1b39171774e7cfa3b532f8bca5
Damian Cyper <63460537+dmncp@users.noreply.github.com>
2022-07-29 17:17:06 +0200

    [SC-119] error handling for login, displaying error message (#85)

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -64,6 +64,7 @@ export const TeacherSidebarTitles = {
 export const FIELD_REQUIRED = 'Pole wymagane.'
 export const POSITIVE_NUMBER = 'Wymagana liczba dodatnia.'
 export const START_GRAPH_NODE_ID = -1
+export const NOT_LOGGED_IN_ERROR = 'Logowanie nieudane. Niepoprawny login lub hasło.'
 
 export const Activity = {
   EXPEDITION: 'EXPEDITION',
