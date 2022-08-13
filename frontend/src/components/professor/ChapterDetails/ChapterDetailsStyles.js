397557713a407a4c22a7bb157440f208870357d2
Damian Cyper <damiancyper@gmail.com>
2022-08-13 22:13:53 +0200

    [SC-169] new student dashboard, chartjs implementation

--- frontend/src/components/professor/ChapterDetails/ChapterDetailsStyles.js
+++ frontend/src/components/professor/ChapterDetails/ChapterDetailsStyles.js
@@ -11,7 +11,7 @@ const ChapterCard = styled(Card)`
   }
 
   .card-body {
-    background-color: #223762;
+    background-color: var(--light-blue);
   }
 `
 
@@ -27,7 +27,7 @@ export const SummaryCard = styled(ChapterCard)`
   overflow-x: hidden;
 
   .list-group-item {
-    background-color: #223762;
+    background-color: var(--light-blue);
   }
 
   #activities,
