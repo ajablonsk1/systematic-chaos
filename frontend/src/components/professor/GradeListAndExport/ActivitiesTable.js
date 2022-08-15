2ba020761ea65bb32e42b41547932619d422fefb
Damian Cyper <damiancyper@gmail.com>
2022-08-15 16:16:52 +0200

    [SC-171] new table inside export modal (mock data) - waiting for endpoints

---
+++ frontend/src/components/professor/GradeListAndExport/ActivitiesTable.js
@@ -0,0 +1,46 @@
+import React from 'react'
+import { getActivitiesList } from './mockData'
+import { Table } from 'react-bootstrap'
+
+function ActivitiesTable(props) {
+  const activitiesList = getActivitiesList()
+
+  const headerInputChecked = (event) => event.target && event.target.checked
+  const inputChecked = (activityId) => props.activitiesToExportIds.includes(activityId)
+  const checkAllRows = (event) =>
+    props.setActivitiesToExportIds(headerInputChecked(event) ? [...activitiesList.map((activity) => activity.id)] : [])
+
+  const checkRow = (event) => {
+    const activityId = +event.target.value
+    inputChecked(activityId)
+      ? props.setActivitiesToExportIds((prevState) => prevState.filter((id) => id !== activityId))
+      : props.setActivitiesToExportIds((prevState) => [...prevState, activityId])
+  }
+
+  return (
+    <Table>
+      <thead>
+        <tr>
+          <th>
+            <input type={'checkbox'} onChange={checkAllRows} />
+          </th>
+          <th>Nazwa aktywności</th>
+          <th>Nazwa rozdziału</th>
+        </tr>
+      </thead>
+      <tbody>
+        {activitiesList.map((activity, index) => (
+          <tr key={index + Date.now()}>
+            <td>
+              <input type={'checkbox'} onChange={checkRow} value={activity.id} checked={inputChecked(activity.id)} />
+            </td>
+            <td>{activity.activityName}</td>
+            <td>{activity.chapterName}</td>
+          </tr>
+        ))}
+      </tbody>
+    </Table>
+  )
+}
+
+export default ActivitiesTable
---
+++ frontend/src/components/professor/GradeListAndExport/mockData.js
@@ -0,0 +1,31 @@
+const activities = [
+  {
+    id: 1,
+    activityName: 'Name 1',
+    chapterName: 'Chapter 1'
+  },
+  {
+    id: 2,
+    activityName: 'Name 2',
+    chapterName: 'Chapter 1'
+  },
+  {
+    id: 3,
+    activityName: 'Name 3',
+    chapterName: 'Chapter 1'
+  },
+  {
+    id: 4,
+    activityName: 'Name 4',
+    chapterName: 'Chapter 1'
+  },
+  {
+    id: 5,
+    activityName: 'Name 5',
+    chapterName: 'Chapter 1'
+  }
+]
+
+export const getActivitiesList = () => {
+  return activities
+}
