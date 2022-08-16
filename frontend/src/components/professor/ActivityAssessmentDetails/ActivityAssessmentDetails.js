871a063cf5ddee462658eeb1c326f3fdff84c3fc
kwdrt <jakubkcichocki@gmail.com>
2022-08-16 18:58:44 +0200

    [SC-117] added unstyled file download

--- frontend/src/components/professor/ActivityAssessmentDetails/ActivityAssessmentDetails.js
+++ frontend/src/components/professor/ActivityAssessmentDetails/ActivityAssessmentDetails.js
@@ -29,92 +29,118 @@ import ProfessorService from '../../../services/professor.service'
 import { Activity } from '../../../utils/constants'
 import Loader from '../../general/Loader/Loader'
 import { generateFullPath, PageRoutes } from '../../../routes/PageRoutes'
+import ActivityAssessmentFileService from './ActivityAssessmentFileService'
 
 export default function ActivityAssessmentDetails() {
-
-  const navigate = useNavigate();
-  const location = useLocation();
-  const { activityId } = location.state;
+  const navigate = useNavigate()
+  const location = useLocation()
+  const { activityId } = location.state
 
   const [activityResponseInfo, setActivityResponseInfo] = useState(undefined)
   const [remarks, setRemarks] = useState('')
   const [givenPoints, setGivenPoints] = useState(0)
-  
+
   useEffect(() => {
-    ProfessorService.getFirstTaskToEvaluate(activityId).then(
-      (activityResponseInfo) => {setActivityResponseInfo(activityResponseInfo); console.log(activityResponseInfo)}
-    )
+    ProfessorService.getFirstTaskToEvaluate(activityId).then((activityResponseInfo) => {
+      setActivityResponseInfo(activityResponseInfo)
+      console.log(activityResponseInfo)
+    })
   }, [activityId])
 
   const sendFeedbackAndGetNextIfAble = () => {
-    ProfessorService.sendTaskEvaluation(activityResponseInfo.fileTaskResponseId, remarks, givenPoints).then((response) => {
-      if(activityResponseInfo.remaining > 0) {
-        setRemarks('');
-        setGivenPoints(0);
-        ProfessorService.getFirstTaskToEvaluate(activityId).then(
-          (activityResponseInfo) => {setActivityResponseInfo(activityResponseInfo); console.log(activityResponseInfo)}
-        )
+    ProfessorService.sendTaskEvaluation(activityResponseInfo.fileTaskResponseId, remarks, givenPoints).then(
+      (response) => {
+        if (activityResponseInfo.remaining > 0) {
+          setRemarks('')
+          setGivenPoints(0)
+          ProfessorService.getFirstTaskToEvaluate(activityId).then((activityResponseInfo) => {
+            setActivityResponseInfo(activityResponseInfo)
+            console.log(activityResponseInfo)
+          })
+        } else {
+          navigate(generateFullPath(() => PageRoutes.Teacher.ActivityAssessment))
+        }
       }
-      else {
-        navigate(generateFullPath(() => PageRoutes.Teacher.ActivityAssessment))
-      }
-    });
+    )
   }
 
   return (
     <Content>
-      {activityResponseInfo ? (<ContentCol>
-        <ActivityTitle>
-          <h4>
-            {getActivityTypeName(Activity.TASK) + ' - ' + activityResponseInfo.activityName}
-          </h4>
-        </ActivityTitle>
-        <TopInfo>
-          <TopInfoCard>
-            <h4 style={{ textAlign: 'center' }}>Informacje o użytkowniku</h4>
-            <FullDivider />
-            <Row styles={{ justifyContent: 'center' }}>
-              <img src={userPicture} alt='profile avatar' style={{ paddingLeft: '20px' }}></img>
-              <UserInfo>
-                <h5 styles={{ width: '100%' }}>{activityResponseInfo.firstName + ' ' + activityResponseInfo.lastName}</h5>
+      {activityResponseInfo ? (
+        <ContentCol>
+          <ActivityTitle>
+            <h4>{getActivityTypeName(Activity.TASK) + ' - ' + activityResponseInfo.activityName}</h4>
+          </ActivityTitle>
+          <TopInfo>
+            <TopInfoCard>
+              <h4 style={{ textAlign: 'center' }}>Informacje o użytkowniku</h4>
+              <FullDivider />
+              <Row styles={{ justifyContent: 'center' }}>
+                <img src={userPicture} alt='profile avatar' style={{ paddingLeft: '20px' }}></img>
+                <UserInfo>
+                  <h5 styles={{ width: '100%' }}>
+                    {activityResponseInfo.firstName + ' ' + activityResponseInfo.lastName}
+                  </h5>
 
-                {/*//TODO: we can do it better, I'm almost sure*/}
-                {!activityResponseInfo.isLate && <h5 styles={{ width: '100%' }}>zadanie oddane w terminie</h5>}
-                {activityResponseInfo.isLate && <h5 styles={{ width: '100%' }}>zadanie oddane ze spóźnieniem</h5>}
-              </UserInfo>
+                  {/*//TODO: we can do it better, I'm almost sure*/}
+                  {!activityResponseInfo.isLate && <h5 styles={{ width: '100%' }}>zadanie oddane w terminie</h5>}
+                  {activityResponseInfo.isLate && <h5 styles={{ width: '100%' }}>zadanie oddane ze spóźnieniem</h5>}
+                </UserInfo>
+              </Row>
+            </TopInfoCard>
+            <TopInfoCard>
+              <h4 style={{ textAlign: 'center' }}>Informacje o aktywności</h4>
+              <FullDivider />
+              <p style={{ marginBottom: '1px' }}>Treść:</p>
+              <ActivityInfo>{activityResponseInfo.activityDetails}</ActivityInfo>
+              <p style={{ textAlign: 'center' }}>Punkty: {activityResponseInfo.maxPoints}</p>
+            </TopInfoCard>
+          </TopInfo>
+          <AnswerRow>
+            <AnswerCol>
+              <h4>Odpowiedź</h4>
+              <AnswerContent>{activityResponseInfo.userAnswer}</AnswerContent>
+              <ActivityAssessmentFileService activityResponseInfo={activityResponseInfo} />
+            </AnswerCol>
+          </AnswerRow>
+
+          <RemarksCol>
+            <h4>Uwagi:</h4>
+            <RemarksTextArea
+              onChange={(newText) => {
+                setRemarks(newText.target.value)
+                console.log(newText.target.value)
+              }}
+            />
+          </RemarksCol>
+
+          <PointsRow>
+            <p style={{ top: '50%', position: 'relative', margin: '0' }}>Punkty: </p>
+            <Row style={{ display: 'flex' }}>
+              <PointsInput
+                type='number'
+                min={0}
+                max={activityResponseInfo.maxPoints}
+                onChange={(newPointsNum) => {
+                  setGivenPoints(newPointsNum.target.value)
+                  console.log(newPointsNum.target.value)
+                }}
+              ></PointsInput>
+              <PointsMax>/ {activityResponseInfo.maxPoints}</PointsMax>
             </Row>
-          </TopInfoCard>
-          <TopInfoCard>
-            <h4 style={{ textAlign: 'center' }}>Informacje o aktywności</h4>
-            <FullDivider />
-            <p style={{ marginBottom: '1px' }}>Treść:</p>
-            <ActivityInfo>{activityResponseInfo.activityDetails}</ActivityInfo>
-            <p style={{ textAlign: 'center' }}>Punkty: {activityResponseInfo.maxPoints}</p>
-          </TopInfoCard>
-        </TopInfo>
-        <AnswerRow>
-          <AnswerCol>
-            <h4>Odpowiedź</h4>
-            <AnswerContent>{activityResponseInfo.userAnswer}</AnswerContent>
-          </AnswerCol>
-        </AnswerRow>
+          </PointsRow>
 
-        <RemarksCol>
-          <h4>Uwagi:</h4>
-          <RemarksTextArea onChange={(newText) => {setRemarks(newText.target.value); console.log(newText.target.value)}}/>
-        </RemarksCol>
-
-        <PointsRow>
-          <p style={{ top: '50%', position: 'relative', margin: '0' }}>Punkty: </p>
-          <Row style={{ display: 'flex' }}>
-            <PointsInput type='number' min={0} max={activityResponseInfo.maxPoints} onChange={(newPointsNum) => {setGivenPoints(newPointsNum.target.value); console.log(newPointsNum.target.value)}}></PointsInput>
-            <PointsMax>/ {activityResponseInfo.maxPoints}</PointsMax>
-          </Row>
-        </PointsRow>
-
-        <AcceptButton onClick={sendFeedbackAndGetNextIfAble} disabled={!givenPoints || givenPoints < 0 || givenPoints > activityResponseInfo.maxPoints}>Zaakceptuj i przejdź do kolejnej odpowiedzi</AcceptButton>
-        <RemainingCount>Pozostało {activityResponseInfo.remaining} odpowiedzi do sprawdzenia</RemainingCount>
-      </ContentCol>) : (<Loader/>)}
+          <AcceptButton
+            onClick={sendFeedbackAndGetNextIfAble}
+            disabled={!givenPoints || givenPoints < 0 || givenPoints > activityResponseInfo.maxPoints}
+          >
+            Zaakceptuj i przejdź do kolejnej odpowiedzi
+          </AcceptButton>
+          <RemainingCount>Pozostało {activityResponseInfo.remaining} odpowiedzi do sprawdzenia</RemainingCount>
+        </ContentCol>
+      ) : (
+        <Loader />
+      )}
     </Content>
   )
 }
