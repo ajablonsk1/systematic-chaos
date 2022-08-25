79d21915ea4a943dcb7866944653983892b7bd2f
Damian Cyper <damiancyper@gmail.com>
2022-08-25 16:42:07 +0200

    [SC-204] badges card - badges list diplaying using table

--- frontend/src/components/professor/GameManagement/RanksAndBadges/RankAndBadgesManagement.js
+++ frontend/src/components/professor/GameManagement/RanksAndBadges/RankAndBadgesManagement.js
@@ -1,14 +1,13 @@
 import React, { useMemo } from 'react'
 import { Content } from '../../../App/AppGeneralStyles'
 import { Button, Col, Row, Tab } from 'react-bootstrap'
-import GameCard from '../../../student/GameCardPage/GameCard'
 import { TabsContainer } from '../../../general/LoginAndRegistrationPage/AuthStyle'
 import { getRanksData } from './mockData'
 import { HeroType } from '../../../../utils/userRole'
 import { getHeroName } from '../../../../utils/constants'
-import { CustomTable } from '../../../student/GameCardPage/gameCardContentsStyle'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faEdit, faTrash } from '@fortawesome/free-solid-svg-icons'
+import { getBadgesList } from '../../../student/BadgesPage/mockData'
+import ContentCard from './ContentCard'
+import Table from './Table'
 
 function RankAndBadgesManagement(props) {
   const ranksData = getRanksData()
@@ -23,34 +22,18 @@ function RankAndBadgesManagement(props) {
             eventKey={rank.heroType}
             title={getHeroName(rank.heroType)}
           >
-            <CustomTable style={{ marginTop: 10 }}>
-              <thead>
-                <tr>
-                  <th>Ikonka</th>
-                  <th>Nazwa rangi</th>
-                  <th>Zakres punktowy</th>
-                </tr>
-              </thead>
-              <tbody>
-                {rank.ranksList.map((rankDetails, i) => (
-                  <tr key={i + Date.now()}>
-                    <td>
-                      <img width={100} src={rankDetails.imgSrc} alt={'rank-icon'} />
-                    </td>
-                    <td className={'align-middle'}>{rankDetails.name}</td>
-                    <td className={'align-middle'}>
-                      {rankDetails.minPoints} - {rankDetails.maxPoints}
-                    </td>
-                    <td className={'align-middle'}>
-                      <FontAwesomeIcon icon={faEdit} />
-                    </td>
-                    <td className={'align-middle'}>
-                      <FontAwesomeIcon icon={faTrash} />
-                    </td>
-                  </tr>
-                ))}
-              </tbody>
-            </CustomTable>
+            <div className={'text-center'} style={{ maxHeight: '74.5vh', overflow: 'auto' }}>
+              <Table
+                headers={['Ikona', 'Nazwa rangi', 'Zakres punktowy']}
+                body={rank.ranksList.map((listElements) => [
+                  <img width={100} src={listElements.imgSrc} alt={'rank-icon'} />,
+                  <span>{listElements.name}</span>,
+                  <span>
+                    {listElements.minPoints} - {listElements.maxPoints}
+                  </span>
+                ])}
+              />
+            </div>
             <Button className={'my-3'}>Dodaj nową rangę</Button>
           </Tab>
         ))}
@@ -58,16 +41,34 @@ function RankAndBadgesManagement(props) {
     )
   }, [ranksData])
 
-  const badgesContent = useMemo(() => {}, [])
+  const badgesContent = useMemo(() => {
+    return (
+      <>
+        <div className={'text-center'} style={{ maxHeight: '93%', overflow: 'auto' }}>
+          <Table
+            headers={['Ikona', 'Nazwa odznaki', 'Opis']}
+            body={getBadgesList().map((badge) => [
+              <img width={100} src={badge.src} alt={'badge-icon'} />,
+              <span>{badge.name}</span>,
+              <span>{badge.description}</span>
+            ])}
+          />
+        </div>
+        <Button className={'my-3'} style={{ position: 'relative', left: '50%', transform: 'translateX(-50%)' }}>
+          Dodaj nową odznakę
+        </Button>
+      </>
+    )
+  }, [])
 
   return (
     <Content>
       <Row className={'m-0 vh-100 w-100'}>
         <Col md={6} className={'pt-4'}>
-          <GameCard headerText={'Rangi'} content={ranksContent} />
+          <ContentCard header={'Rangi'} body={ranksContent} />
         </Col>
         <Col md={6} className={'pt-4'}>
-          <GameCard headerText={'Odznaki'} content={badgesContent} />
+          <ContentCard header={'Odznaki'} body={badgesContent} />
         </Col>
       </Row>
     </Content>
