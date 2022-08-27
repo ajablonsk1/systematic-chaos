9b634dac4de746faec542c85a0be65522ad29f99
kwdrt <jakubkcichocki@gmail.com>
2022-08-27 12:43:48 +0200

    [SC-189] draft of the professor screen

--- frontend/src/components/professor/ActivityAssessmentDetails/ActivityAssesmentDetailsStyles.js
+++ frontend/src/components/professor/ActivityAssessmentDetails/ActivityAssesmentDetailsStyles.js
@@ -1,6 +1,40 @@
 import styled from 'styled-components'
 import { Button, Col, Row } from 'react-bootstrap'
 
+export const HeaderRow = styled(Row)`
+  background-color: var(--dark-blue);
+  color: var(--font-color);
+`
+
+export const ActivityImg = styled.img`
+  height: 40px;
+  width: 40px;
+  margin: 10px;
+
+  @media (max-width: 800px) {
+    height: 20px;
+    width: 20px;
+    margin: 5px;
+  } ;
+`
+
+export const ActivityType = styled.h1`
+  text-align: left;
+  @media (max-width: 800px) {
+    font-size: 1.5rem;
+  } ;
+`
+
+export const ActivityName = styled.h1`
+  text-align: right;
+  margin-left: auto;
+  @media (max-width: 800px) {
+    font-size: 1.5rem;
+  } ;
+`
+
+///prev from here
+
 export const ContentCol = styled(Col)`
   padding-top: 15px;
   margin: 0 auto;
--- frontend/src/components/professor/ActivityAssessmentDetails/ActivityAssessmentDetails.js
+++ frontend/src/components/professor/ActivityAssessmentDetails/ActivityAssessmentDetails.js
@@ -1,15 +1,13 @@
 import { Content } from '../../App/AppGeneralStyles'
-import { Row } from 'react-bootstrap'
+import { Row, Col } from 'react-bootstrap'
 import { FullDivider } from '../../student/AllActivities/ExpeditionTask/ActivityInfo/ActivityInfoStyles'
 //import { useLocation, useNavigate } from 'react-router-dom';
 import {
   AcceptButton,
   ActivityInfo,
-  ActivityTitle,
   AnswerCol,
   AnswerContent,
   AnswerRow,
-  ContentCol,
   PointsInput,
   PointsMax,
   PointsRow,
@@ -19,20 +17,23 @@ import {
   TopInfo,
   TopInfoCard,
   UserInfo,
-  ActivityAssesmentProfessorFileCol
+  ActivityAssesmentProfessorFileCol,
+  HeaderRow,
+  ActivityImg,
+  ActivityName,
+  ActivityType
 } from './ActivityAssesmentDetailsStyles'
-import { getActivityTypeName } from '../../../utils/constants'
 //https://www.flaticon.com/free-icon/user-picture_21104
 import userPicture from '../../../utils/resources/user-picture.png'
 import { useEffect, useRef, useState, useMemo } from 'react'
 import { useNavigate, useLocation } from 'react-router-dom'
 import ProfessorService from '../../../services/professor.service'
-import { Activity } from '../../../utils/constants'
 import Loader from '../../general/Loader/Loader'
 import { generateFullPath, PageRoutes } from '../../../routes/PageRoutes'
 import ActivityAssessmentStudentFileService from './ActivityAssessmentStudentFileService'
 import { ActivityAssessmentProfessorFileService } from './ActivityAssessmentProfessorFileService'
 import { debounce } from 'lodash'
+import { getActivityImg, getActivityTypeName } from '../../../utils/constants'
 
 export default function ActivityAssessmentDetails() {
   const navigate = useNavigate()
@@ -114,36 +115,129 @@ export default function ActivityAssessmentDetails() {
       .catch(() => {})
   }
 
-  return (
-    <Content>
-      {activityResponseInfo ? (
-        <ContentCol>
-          <ActivityTitle>
-            <h4>{getActivityTypeName(Activity.TASK) + ' - ' + activityResponseInfo.activityName}</h4>
-          </ActivityTitle>
-          <TopInfo>
-            <TopInfoCard>
-              <h4 className={'text-center'}>Informacje o użytkowniku</h4>
-              <FullDivider />
-              <Row>
-                <img src={userPicture} alt='profile avatar' style={{ paddingLeft: '20px', height: '100px' }}></img>
-                <UserInfo>
-                  <h5>{activityResponseInfo.firstName + ' ' + activityResponseInfo.lastName}</h5>
+  // const originalContent = () => {
+  //   return (
+  //     <ContentCol>
+  //       <ActivityTitle>
+  //         <h4>{getActivityTypeName(Activity.TASK) + ' - ' + activityResponseInfo.activityName}</h4>
+  //       </ActivityTitle>
+  //       <TopInfo>
+  //         <TopInfoCard>
+  //           <h4 className={'text-center'}>Informacje o użytkowniku</h4>
+  //           <FullDivider />
+  //           <Row>
+  //             <img src={userPicture} alt='profile avatar' style={{ paddingLeft: '20px', height: '100px' }}></img>
+  //             <UserInfo>
+  //               <h5>{activityResponseInfo.firstName + ' ' + activityResponseInfo.lastName}</h5>
 
-                  {activityResponseInfo.isLate ? (
-                    <h5 className={'w-100'}>zadanie oddane ze spóźnieniem</h5>
-                  ) : (
-                    <h5 className={'w-100'}>zadanie oddane w terminie</h5>
-                  )}
-                </UserInfo>
-              </Row>
-            </TopInfoCard>
-            <TopInfoCard>
-              <h4 className={'text-center'}>Informacje o aktywności</h4>
-              <FullDivider />
-              <ActivityInfo>{'Treść: ' + activityResponseInfo.activityDetails}</ActivityInfo>
-              <p className='text-center'>Punkty: {activityResponseInfo.maxPoints}</p>
-            </TopInfoCard>
+  //               {activityResponseInfo.isLate ? (
+  //                 <h5 className={'w-100'}>zadanie oddane ze spóźnieniem</h5>
+  //               ) : (
+  //                 <h5 className={'w-100'}>zadanie oddane w terminie</h5>
+  //               )}
+  //             </UserInfo>
+  //           </Row>
+  //         </TopInfoCard>
+  //         <TopInfoCard>
+  //           <h4 className={'text-center'}>Informacje o aktywności</h4>
+  //           <FullDivider />
+  //           <ActivityInfo>{'Treść: ' + activityResponseInfo.activityDetails}</ActivityInfo>
+  //           <p className='text-center'>Punkty: {activityResponseInfo.maxPoints}</p>
+  //         </TopInfoCard>
+  //       </TopInfo>
+  //       <AnswerRow>
+  //         <AnswerCol>
+  //           <h4>Odpowiedź</h4>
+  //           <AnswerContent>{activityResponseInfo.userAnswer}</AnswerContent>
+  //           <div>
+  //             <ActivityAssessmentStudentFileService activityResponseInfo={activityResponseInfo} />
+  //           </div>
+  //         </AnswerCol>
+  //       </AnswerRow>
+
+  //       <RemarksCol>
+  //         <h4>Uwagi:</h4>
+  //         <RemarksTextArea onChange={debounceSetText} ref={textRef} />
+  //       </RemarksCol>
+
+  //       <ActivityAssesmentProfessorFileCol>
+  //         <ActivityAssessmentProfessorFileService setFile={setFileBlob} setFileName={setFileName} fileRef={fileRef} />
+  //       </ActivityAssesmentProfessorFileCol>
+
+  //       <PointsRow>
+  //         <p className='position-relative m-0 top-50'>Punkty: </p>
+  //         <Row className={'d-flex'}>
+  //           <PointsInput
+  //             type='number'
+  //             min={0}
+  //             max={activityResponseInfo.maxPoints}
+  //             onChange={debounceSetGivenPoints}
+  //             ref={pointRef}
+  //           ></PointsInput>
+  //           <PointsMax>/ {activityResponseInfo.maxPoints}</PointsMax>
+  //         </Row>
+  //       </PointsRow>
+
+  //       <AcceptButton
+  //         onClick={sendFeedbackAndGetNextIfAble}
+  //         disabled={!givenPoints || givenPoints < 0 || givenPoints > activityResponseInfo.maxPoints}
+  //       >
+  //         Zaakceptuj i przejdź do kolejnej odpowiedzi
+  //       </AcceptButton>
+  //       <RemainingCount>Pozostało {activityResponseInfo.remaining} odpowiedzi do sprawdzenia</RemainingCount>
+  //     </ContentCol>
+  //   )
+  // }
+
+  const HorizontalSpacer = () => <Col style={{ height: '3vh' }} />
+
+  const VerticalSpacer = () => <Row style={{ height: '2vh' }}></Row>
+
+  const Header = () => (
+    <>
+      <ActivityImg src={getActivityImg('TASK')}></ActivityImg>
+      <ActivityType>{getActivityTypeName('TASK')}</ActivityType>
+      <ActivityName>{activityResponseInfo.name}</ActivityName>
+    </>
+  )
+
+  const content = () => {
+    return (
+      <>
+        <HorizontalSpacer />
+        <Col
+          className='m-0 pt-4 mx-auto'
+          style={{ height: '94vh', width: '90%', backgroundColor: 'var(--light-blue)' }}
+        >
+          <HeaderRow className='p-2 rounded mx-2' style={{ height: '8vh' }}>
+            <Header />
+          </HeaderRow>
+
+          <VerticalSpacer />
+
+          <TopInfo>
+            {/* <TopInfoCard>
+            <h4 className={'text-center'}>Informacje o użytkowniku</h4>
+            <FullDivider />
+            <Row>
+              <img src={userPicture} alt='profile avatar' style={{ paddingLeft: '20px', height: '100px' }}></img>
+              <UserInfo>
+                <h5>{activityResponseInfo.firstName + ' ' + activityResponseInfo.lastName}</h5>
+
+                {activityResponseInfo.isLate ? (
+                  <h5 className={'w-100'}>zadanie oddane ze spóźnieniem</h5>
+                ) : (
+                  <h5 className={'w-100'}>zadanie oddane w terminie</h5>
+                )}
+              </UserInfo>
+            </Row>
+          </TopInfoCard>
+          <TopInfoCard>
+            <h4 className={'text-center'}>Informacje o aktywności</h4>
+            <FullDivider />
+            <ActivityInfo>{'Treść: ' + activityResponseInfo.activityDetails}</ActivityInfo>
+            <p className='text-center'>Punkty: {activityResponseInfo.maxPoints}</p>
+          </TopInfoCard> */}
           </TopInfo>
           <AnswerRow>
             <AnswerCol>
@@ -185,10 +279,11 @@ export default function ActivityAssessmentDetails() {
             Zaakceptuj i przejdź do kolejnej odpowiedzi
           </AcceptButton>
           <RemainingCount>Pozostało {activityResponseInfo.remaining} odpowiedzi do sprawdzenia</RemainingCount>
-        </ContentCol>
-      ) : (
-        <Loader />
-      )}
-    </Content>
-  )
+        </Col>
+        <HorizontalSpacer />
+      </>
+    )
+  }
+
+  return <Content>{activityResponseInfo ? content() : <Loader />}</Content>
 }
