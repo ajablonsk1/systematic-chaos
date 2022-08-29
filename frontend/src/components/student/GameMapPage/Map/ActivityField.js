22eacc6c7db152f23c1284b36c0f0b618490c7c3
Damian Cyper <damiancyper@gmail.com>
2022-08-29 17:15:25 +0200

    [SC-216] offcanvas panel for activities map

--- frontend/src/components/student/GameMapPage/Map/ActivityField.js
+++ frontend/src/components/student/GameMapPage/Map/ActivityField.js
@@ -1,11 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { getActivityByPosition } from '../../../../storage/activityMap'
-import { getActivityImg, getActivityPath } from '../../../../utils/constants'
-import { ActivityCol } from './ActivityFieldStyle'
+import { getActivityImg, getActivityPath, getActivityTypeName } from '../../../../utils/constants'
+import { ActivityCol, CustomOffcanvas, OffcanvasTable } from './ActivityFieldStyle'
+import { Button, Offcanvas, OffcanvasBody, OffcanvasHeader, OffcanvasTitle } from 'react-bootstrap'
+import moment from 'moment'
+import { getRequirements } from './mockData'
 
 export default function ActivityField({ activity, posX, posY, colClickable, colSize }) {
   const navigate = useNavigate()
+  const [isOffcanvasOpen, setIsOffcanvasOpen] = useState(false)
+  const requirements = getRequirements()
 
   // TODO, currently goes to the hard-coded expedition activity but it should be OK once we implement a 'real' activity getter in API
   const startActivity = () => {
@@ -27,18 +32,66 @@ export default function ActivityField({ activity, posX, posY, colClickable, colS
     }
     return false
   }
-
+  console.log(activity)
   return (
-    <ActivityCol $isClickable={colClickable} $colSize={colSize}>
-      {activity ? (
-        <img src={getActivityImg(activity.type)} alt='activityImg' onClick={startActivity} />
-      ) : (
-        <div
-          style={{
-            backgroundColor: isCompletedActivityAround() ? 'transparent' : 'white'
-          }}
-        />
-      )}
-    </ActivityCol>
+    <>
+      <ActivityCol $isClickable={colClickable} $colSize={colSize}>
+        {activity ? (
+          <img src={getActivityImg(activity.type)} alt='activityImg' onClick={() => setIsOffcanvasOpen(true)} />
+        ) : (
+          <div
+            style={{
+              backgroundColor: isCompletedActivityAround() ? 'transparent' : 'white'
+            }}
+          />
+        )}
+      </ActivityCol>
+      <CustomOffcanvas placement={'end'} show={isOffcanvasOpen} onHide={() => setIsOffcanvasOpen(false)}>
+        <OffcanvasHeader closeButton>
+          <OffcanvasTitle>Szczegóły aktywności</OffcanvasTitle>
+        </OffcanvasHeader>
+        <OffcanvasBody>
+          <table>
+            <tbody>
+              <tr>
+                <th colSpan={2}>Podstawowe informacje</th>
+              </tr>
+              <tr>
+                <td>Nazwa aktywności:</td>
+                <td>{activity?.title}</td>
+              </tr>
+              <tr>
+                <td>Typ aktywności:</td>
+                <td>{getActivityTypeName(activity?.type)}</td>
+              </tr>
+              <tr>
+                <td>Punkty:</td>
+                <td>{activity?.points}</td>
+              </tr>
+              <tr>
+                <td>Data utworzenia:</td>
+                <td>{moment(Date.now()).format('DD.MM.YYYY, HH:mm')}</td>
+              </tr>
+              <tr>
+                <th colSpan={2}>Wymagania odblokowania</th>
+              </tr>
+              {requirements.map((requirement, index) => (
+                <tr key={index + Date.now()}>
+                  <td>{requirement.name}:</td>
+                  <td>{requirement.value}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+          <Button
+            variant={'outline-warning'}
+            className={'position-relative start-50 translate-middle-x mt-3'}
+            onClick={startActivity}
+          >
+            Rozpocznij
+          </Button>
+        </OffcanvasBody>
+      </CustomOffcanvas>
+    </>
   )
 }
--- frontend/src/components/student/GameMapPage/Map/ActivityFieldStyle.js
+++ frontend/src/components/student/GameMapPage/Map/ActivityFieldStyle.js
@@ -1,5 +1,5 @@
 import styled, { css } from 'styled-components'
-import { Col } from 'react-bootstrap'
+import { Col, Offcanvas } from 'react-bootstrap'
 
 export const ActivityCol = styled(Col)`
   border: 2px black solid;
@@ -26,4 +26,55 @@ export const ActivityCol = styled(Col)`
         transform: scale(1.1);
       }
     `}
+`
+
+export const CustomOffcanvas = styled(Offcanvas)`
+  background-color: var(--light-blue);
+  color: var(--font-color);
+
+  .btn-close {
+    filter: invert(81%) sepia(21%) saturate(5299%) hue-rotate(351deg) brightness(101%) contrast(101%);
+  }
+
+  table {
+    border-top: 1px solid var(--font-color);
+    border-spacing: 5px;
+    width: 100%;
+
+    tbody {
+      tr {
+        word-break: break-all;
+      }
+
+      th,
+      td {
+        padding: 10px 0;
+        color: lightgrey;
+      }
+
+      td:first-child {
+        width: 55%;
+      }
+
+      tr:nth-of-type(5) {
+        border-bottom: 1px solid var(--font-color);
+      }
+
+      td:nth-child(odd) {
+        white-space: nowrap;
+        color: var(--font-color);
+      }
+
+      td:nth-child(even) {
+        text-align: right;
+      }
+
+      th {
+        padding-top: 15px;
+        font-size: 1.1rem;
+        font-weight: 700;
+        color: var(--font-color);
+      }
+    }
+  }
 `
