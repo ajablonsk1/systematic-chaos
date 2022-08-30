97c8c9f8c82d88c3198da92f9333255d401b19aa
Damian Cyper <63460537+dmncp@users.noreply.github.com>
2022-08-30 17:52:57 +0200

    Sc 218 zbudowanie widoku wytycznych od nowa (#148)
    * [SC-218] responsive grid layout for images gallery, basic functionality
    
    * [SC-218] code refactor and fix some bugs
    
    * [SC-218] some style changes, full preview for images, downloading functionality
    
    * [SC-218] code refactor

--- frontend/src/components/student/AllActivities/InfoTask/Information.js
+++ frontend/src/components/student/AllActivities/InfoTask/Information.js
@@ -1,27 +1,21 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useMemo, useRef, useState } from 'react'
 import { useLocation } from 'react-router-dom'
 import { Activity, ERROR_OCCURRED, getActivityImg, getActivityTypeName } from '../../../../utils/constants'
-import {
-  ActivityCol,
-  ActivityImg,
-  ActivityName,
-  ActivityType,
-  FullDivider,
-  HeaderCol,
-  HeaderRow,
-  SmallDivider
-} from '../ExpeditionTask/ActivityInfo/ActivityInfoStyles'
-import { InfoContainer } from '../ExpeditionTask/ActivityInfo/InfoContainer'
 import { Content } from '../../../App/AppGeneralStyles'
-import { InformationImage } from './InformationStyles'
+import { InformationTable } from './InformationStyles'
 import Loader from '../../../general/Loader/Loader'
 import InfoTaskService from '../../../../services/infoTask.service'
+import { Card, Col, Row } from 'react-bootstrap'
+import { CustomCard } from '../../GameCardPage/GameCardStyles'
+import CardHeader from 'react-bootstrap/CardHeader'
+import ImagesGallery from '../../../general/ImagesGallery/ImagesGallery'
 
 export default function Information() {
   const location = useLocation()
   const { activityId: informationId } = location.state
 
   const [information, setInformation] = useState(undefined)
+  const gridCardRef = useRef()
 
   useEffect(() => {
     InfoTaskService.getInformation(informationId)
@@ -29,37 +23,93 @@ export default function Information() {
       .catch(() => setInformation(null))
   }, [informationId])
 
+  const activityInfoCardBody = useMemo(() => {
+    if (information === undefined) {
+      return <Loader />
+    }
+    if (information == null) {
+      return <p>{ERROR_OCCURRED}</p>
+    }
+
+    const tableElements = [
+      { name: 'Typ aktywności', value: getActivityTypeName(Activity.INFO) },
+      { name: 'Nazwa', value: information.name },
+      { name: 'Opis', value: 'Tutaj będzie krótki opis gdy backend to doda' }
+    ]
+
+    return (
+      <InformationTable>
+        <tbody>
+          <tr>
+            <td>Ikonka aktywności:</td>
+            <td>
+              <img height={45} src={getActivityImg(Activity.INFO)} alt={'info-task-icon'} />
+            </td>
+          </tr>
+          {tableElements.map((element, index) => (
+            <tr key={index + Date.now()}>
+              <td>{element.name}:</td>
+              <td>{element.value}</td>
+            </tr>
+          ))}
+        </tbody>
+      </InformationTable>
+    )
+  }, [information])
+
+  const imagesGridLayout = useMemo(() => {
+    if (information === undefined) {
+      return <Loader />
+    }
+    if (information === null) {
+      return <p>{ERROR_OCCURRED}</p>
+    }
+
+    if (gridCardRef.current?.offsetWidth) {
+      return <ImagesGallery width={gridCardRef.current.offsetWidth - 32} images={information.imageUrls} cols={3} />
+    }
+
+    return <></>
+  }, [information])
+
   return (
-    <>
-      {information === undefined ? (
-        <Loader />
-      ) : information == null ? (
-        <p className={'text-center text-danger h4'}>{ERROR_OCCURRED}</p>
-      ) : (
-        <Content>
-          <InfoContainer>
-            <ActivityCol>
-              <HeaderCol>
-                <HeaderRow>
-                  <ActivityImg src={getActivityImg(Activity.INFO)}></ActivityImg>
-                  <ActivityType>{getActivityTypeName(Activity.INFO)}</ActivityType>
-                  <ActivityName>{information.name}</ActivityName>
-                </HeaderRow>
-                <FullDivider />
-              </HeaderCol>
-              <div>
-                {information.imageUrls.map((image, idx) => (
-                  <InformationImage src={image} alt={idx} key={idx} />
-                ))}
-              </div>
-              {information.imageUrls.length ? <SmallDivider /> : <></>}
-              <div>
-                <p>{information.description}</p>
-              </div>
-            </ActivityCol>
-          </InfoContainer>
-        </Content>
-      )}
-    </>
+    <Content>
+      <Row className={'m-0 vh-100'}>
+        <Col md={6} className={'py-2'}>
+          <Row className={'m-0 w-100 h-25 pb-2'}>
+            <CustomCard className={'p-0'}>
+              <CardHeader>
+                <h5>Informacje o aktywności</h5>
+              </CardHeader>
+              <Card.Body>{activityInfoCardBody}</Card.Body>
+            </CustomCard>
+          </Row>
+          <Row className={'m-0 w-100 h-75'}>
+            <CustomCard className={'p-0'}>
+              <CardHeader>
+                <h5>Opis</h5>
+              </CardHeader>
+              <Card.Body className={'overflow-auto'} style={{ maxHeight: '70vh' }}>
+                {information === undefined ? (
+                  <Loader />
+                ) : information == null ? (
+                  <p>{ERROR_OCCURRED}</p>
+                ) : (
+                  <p>{information.description}</p>
+                )}
+              </Card.Body>
+            </CustomCard>
+          </Row>
+        </Col>
+        <Col md={6} className={'py-2'}>
+          <CustomCard className={'p-0'} style={{ maxHeight: '98.3vh', overflowY: 'auto' }}>
+            <CardHeader className={'position-sticky top-0'} style={{ zIndex: 2 }}>
+              <h5>Galeria zdjęć</h5>
+            </CardHeader>
+            <Card.Body ref={gridCardRef}>{imagesGridLayout}</Card.Body>
+          </CustomCard>
+        </Col>
+      </Row>
+    </Content>
   )
 }
