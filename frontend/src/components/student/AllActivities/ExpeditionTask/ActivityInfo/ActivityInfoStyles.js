34f137da5a38b42b47a8795477afc100f3f198f1
Damian Cyper <damiancyper@gmail.com>
2022-09-03 17:46:37 +0200

    [SC-219] linter fix

--- frontend/src/components/student/AllActivities/ExpeditionTask/ActivityInfo/ActivityInfoStyles.js
+++ frontend/src/components/student/AllActivities/ExpeditionTask/ActivityInfo/ActivityInfoStyles.js
@@ -1,5 +1,5 @@
 import styled from 'styled-components'
-import { Button, Col, Row } from 'react-bootstrap'
+import { Col, Row } from 'react-bootstrap'
 
 export const ActivityImg = styled.img`
   height: 30px;
