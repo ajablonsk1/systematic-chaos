0c8864fef65a5315d7bf94fe284d5827f413c090
Damian Cyper <damiancyper@gmail.com>
2022-09-04 20:20:00 +0200

    [SC-214] using endpoint to displaying game summary stats

--- frontend/src/components/professor/GameSummary/GameSummary.js
+++ frontend/src/components/professor/GameSummary/GameSummary.js
@@ -1,9 +1,8 @@
-import React, { useCallback, useState } from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
 import { Content } from '../../App/AppGeneralStyles'
 import { Card, Carousel, CarouselItem, Col, Row } from 'react-bootstrap'
 import { CustomCard } from '../../student/GameCardPage/GameCardStyles'
 import CardHeader from 'react-bootstrap/CardHeader'
-import { getGameSummaryInfo } from './mockData'
 import GameSummaryCard from './GameSummaryCard'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faArrowLeft, faArrowRight } from '@fortawesome/free-solid-svg-icons'
@@ -23,42 +22,55 @@ import {
   Tooltip
 } from 'chart.js'
 import { lineChartConfig } from './lineChartHelper'
+import ProfessorService from '../../../services/professor.service'
+import Loader from '../../general/Loader/Loader'
+import { ERROR_OCCURRED, getActivityTypeName } from '../../../utils/constants'
 
 ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend, ArcElement, PointElement, LineElement)
 
 export default function GameSummary() {
-  const summaryDetails = getGameSummaryInfo()
+  const [summaryDetails, setSummaryDetails] = useState(undefined)
 
   const [barChartActiveChapterId, setBarChartActiveChapterId] = useState(0)
   const [lineChartActiveChapterId, setLineChartActiveChapterId] = useState(0)
 
+  useEffect(() => {
+    ProfessorService.getGameSummaryStats()
+      .then((response) => {
+        setSummaryDetails(response)
+      })
+      .catch(() => {
+        setSummaryDetails(null)
+      })
+  }, [])
+
   const gradesStatsCardTitles = [
-    { text: 'Średnia ocen studentów:', value: summaryDetails.avgGrade },
-    { text: 'Mediana ocen studentów', value: summaryDetails.medianGrade },
-    { text: 'Aktywność z najlepszym wynikiem średnim:', value: summaryDetails.bestScoreActivityName },
-    { text: 'Aktywność z najgorszym wynikiem średnim:', value: summaryDetails.worstScoreActivityName }
+    { text: 'Średnia ocen studentów:', value: summaryDetails?.avgGrade },
+    { text: 'Mediana ocen studentów', value: summaryDetails?.medianGrade },
+    { text: 'Aktywność z najlepszym wynikiem średnim:', value: summaryDetails?.bestScoreActivityName },
+    { text: 'Aktywność z najgorszym wynikiem średnim:', value: summaryDetails?.worstScoreActivityName }
   ]
 
   const activityStatsCardTitles = [
-    { text: 'Liczba ocenionych aktywności:', value: summaryDetails.assessedActivitiesCounter },
-    { text: 'Liczba nieocenionych aktywności:', value: summaryDetails.notAssessedActivityCounter },
-    { text: 'Liczba odpowiedzi oczekujących na sprawdzenie:', value: summaryDetails.waitingAnswersNumber }
+    { text: 'Liczba ocenionych aktywności:', value: summaryDetails?.assessedActivitiesCounter },
+    { text: 'Liczba nieocenionych aktywności:', value: summaryDetails?.notAssessedActivityCounter },
+    { text: 'Liczba odpowiedzi oczekujących na sprawdzenie:', value: summaryDetails?.waitingAnswersNumber }
   ]
 
   const { data: barChartData, options: barChartOptions } = getChartConfig('BAR', {
-    mainLabels: summaryDetails.avgGradesList[barChartActiveChapterId].avgGradesForChapter.map(
+    mainLabels: summaryDetails?.avgGradesList[barChartActiveChapterId]?.avgGradesForChapter.map(
       (grades) => grades.groupName
     ),
     labels: ['Średnia', 'Mediana'],
     dataset: [
-      summaryDetails.avgGradesList[barChartActiveChapterId].avgGradesForChapter.map((grades) => grades.avgGrade),
-      summaryDetails.avgGradesList[barChartActiveChapterId].avgGradesForChapter.map((grades) => grades.medianGrade)
+      summaryDetails?.avgGradesList[barChartActiveChapterId]?.avgGradesForChapter.map((grades) => grades.avgGrade),
+      summaryDetails?.avgGradesList[barChartActiveChapterId]?.avgGradesForChapter.map((grades) => grades.medianGrade)
     ],
     xAxisDisplay: true
   })
 
   const { data: lineChartData, options: lineChartOptions } = lineChartConfig(
-    summaryDetails.avgActivitiesScore[lineChartActiveChapterId]?.activitiesScore
+    summaryDetails?.avgActivitiesScore[lineChartActiveChapterId]?.activitiesScore
   )
 
   const statsCardBody = useCallback((titles) => {
@@ -73,7 +85,7 @@ export default function GameSummary() {
   const carousel = useCallback(
     (onSelectCallback) => {
       const getChapterNames = () => {
-        return summaryDetails.avgGradesList.map((chapter) => chapter.chapterName)
+        return summaryDetails?.avgGradesList.map((chapter) => chapter.chapterName)
       }
 
       return (
@@ -95,80 +107,94 @@ export default function GameSummary() {
         </Carousel>
       )
     },
-    [summaryDetails.avgGradesList]
+    [summaryDetails]
   )
 
   return (
     <Content>
-      <Row className={'m-0'} style={{ height: '50vh' }}>
-        <Col md={6}>
-          <Row className={'h-50 py-2'}>
-            <GameSummaryCard header={<h5>Statystyki ocen studentów</h5>} body={statsCardBody(gradesStatsCardTitles)} />
+      {summaryDetails === undefined ? (
+        <Loader />
+      ) : summaryDetails == null ? (
+        <p className={'text-center h3'}>{ERROR_OCCURRED}</p>
+      ) : (
+        <>
+          <Row className={'m-0'} style={{ height: '50vh' }}>
+            <Col md={6}>
+              <Row className={'h-50 py-2'}>
+                <GameSummaryCard
+                  header={<h5>Statystyki ocen studentów</h5>}
+                  body={statsCardBody(gradesStatsCardTitles)}
+                />
+              </Row>
+              <Row className={'h-50 py-2'}>
+                <GameSummaryCard
+                  header={<h5>Statystyki aktywności</h5>}
+                  body={statsCardBody(activityStatsCardTitles)}
+                />
+              </Row>
+            </Col>
+            <Col md={6} className={'py-2'}>
+              <CustomCard>
+                <CardHeader>
+                  <h5>Średnia ocen w każdej grupie</h5>
+                </CardHeader>
+                <Card.Body>
+                  {carousel(setBarChartActiveChapterId)}
+                  <ChartCol className={'top-50 translate-middle-y'}>
+                    <Bar data={barChartData} options={barChartOptions} />
+                  </ChartCol>
+                </Card.Body>
+              </CustomCard>
+            </Col>
           </Row>
-          <Row className={'h-50 py-2'}>
-            <GameSummaryCard header={<h5>Statystyki aktywności</h5>} body={statsCardBody(activityStatsCardTitles)} />
+          <Row className={'m-0'} style={{ height: '50vh' }}>
+            <Col md={6} className={'py-2'}>
+              <CustomCard>
+                <CardHeader>
+                  <h5>Średni wynik z aktywności</h5>
+                </CardHeader>
+                <Card.Body style={{ maxHeight: '42vh' }}>
+                  {carousel(setLineChartActiveChapterId)}
+                  <ChartCol className={'top-50 translate-middle-y'}>
+                    {lineChartData && lineChartOptions ? (
+                      <Line data={lineChartData} options={lineChartOptions} />
+                    ) : (
+                      <p>Nie można narysować wykresu</p>
+                    )}
+                  </ChartCol>
+                </Card.Body>
+              </CustomCard>
+            </Col>
+            <Col md={6} className={'py-2'}>
+              <CustomCard>
+                <CardHeader>
+                  <h5>Nieocenione aktywności</h5>
+                </CardHeader>
+                <Card.Body style={{ maxHeight: '42vh', overflow: 'auto' }}>
+                  <CustomTable>
+                    <thead className={'position-sticky'} style={{ top: '-5%', background: 'var(--light-blue)' }}>
+                      <tr>
+                        <th>Nazwa aktywności</th>
+                        <th>Typ aktywności</th>
+                        <th>Liczba odpowiedzi</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      {summaryDetails.notAssessedActivitiesTable.map((activity, index) => (
+                        <tr key={index + Date.now()}>
+                          <td>{activity.activityName}</td>
+                          <td>{getActivityTypeName(activity.activityType)}</td>
+                          <td>{activity.waitingAnswersNumber}</td>
+                        </tr>
+                      ))}
+                    </tbody>
+                  </CustomTable>
+                </Card.Body>
+              </CustomCard>
+            </Col>
           </Row>
-        </Col>
-        <Col md={6} className={'py-2'}>
-          <CustomCard>
-            <CardHeader>
-              <h5>Średnia ocen w każdej grupie</h5>
-            </CardHeader>
-            <Card.Body>
-              {carousel(setBarChartActiveChapterId)}
-              <ChartCol className={'top-50 translate-middle-y'}>
-                <Bar data={barChartData} options={barChartOptions} />
-              </ChartCol>
-            </Card.Body>
-          </CustomCard>
-        </Col>
-      </Row>
-      <Row className={'m-0'} style={{ height: '50vh' }}>
-        <Col md={6} className={'py-2'}>
-          <CustomCard>
-            <CardHeader>
-              <h5>Średni wynik z aktywności</h5>
-            </CardHeader>
-            <Card.Body style={{ maxHeight: '42vh' }}>
-              {carousel(setLineChartActiveChapterId)}
-              <ChartCol className={'top-50 translate-middle-y'}>
-                {lineChartData && lineChartOptions ? (
-                  <Line data={lineChartData} options={lineChartOptions} />
-                ) : (
-                  <p>Nie można narysować wykresu</p>
-                )}
-              </ChartCol>
-            </Card.Body>
-          </CustomCard>
-        </Col>
-        <Col md={6} className={'py-2'}>
-          <CustomCard>
-            <CardHeader>
-              <h5>Nieocenione aktywności</h5>
-            </CardHeader>
-            <Card.Body style={{ maxHeight: '42vh', overflow: 'auto' }}>
-              <CustomTable>
-                <thead className={'position-sticky'} style={{ top: '-5%', background: 'var(--light-blue)' }}>
-                  <tr>
-                    <th>Nazwa aktywności</th>
-                    <th>Typ aktywności</th>
-                    <th>Liczba odpowiedzi</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {summaryDetails.notAssessedActivitiesTable.map((activity, index) => (
-                    <tr key={index + Date.now()}>
-                      <td>{activity.activityName}</td>
-                      <td>{activity.activityType}</td>
-                      <td>{activity.waitingAnswersNumber}</td>
-                    </tr>
-                  ))}
-                </tbody>
-              </CustomTable>
-            </Card.Body>
-          </CustomCard>
-        </Col>
-      </Row>
+        </>
+      )}
     </Content>
   )
 }
