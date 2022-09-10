1e5afca50658640f06712308eef8a0775c985523
kwdrt <jakubkcichocki@gmail.com>
2022-09-10 14:35:17 +0200

    [SC-226] refactored the picker to another file

--- frontend/src/components/professor/GameManagement/AddChapterModal/AddChapterModal.js
+++ frontend/src/components/professor/GameManagement/AddChapterModal/AddChapterModal.js
@@ -10,18 +10,12 @@ import {
 import { FormCol } from '../../../general/LoginAndRegistrationPage/FormCol'
 import ChapterService from '../../../../services/chapter.service'
 import { SuccessModal } from '../../SuccessModal'
-import GridLayout from 'react-grid-layout'
-import { ImageContainer } from '../../../general/ImagesGallery/ImagesGalleryStyle'
+import { BackgroundImagePicker } from './BackgroundImagePicker'
 
-const IMAGE_SELECT_COLUMNS = 3
-const IMAGE_SELECT_ROW_HEIGHT = 300
-const IMAGE_HEIGHT = 200
-const IMAGE_WIDTH = 300
 export function AddChapterModal({ showModal, setShowModal, refetchChapterList }) {
   const [isSuccessModalOpen, setIsSuccessModalOpen] = useState(false)
   const [errorMessage, setErrorMessage] = useState('')
-  const [layout, setLayout] = useState(undefined)
-  const [imagesHeight, setImagesHeight] = useState([])
+
   const images = [
     'https://upload.wikimedia.org/wikipedia/commons/thumb/7/7a/Wikipe-tan_full_length.svg/800px-Wikipe-tan_full_length.svg.png',
     'https://upload.wikimedia.org/wikipedia/commons/1/1f/Wiki-sisters.png',
@@ -29,33 +23,6 @@ export function AddChapterModal({ showModal, setShowModal, refetchChapterList })
     'https://upload.wikimedia.org/wikipedia/commons/thumb/7/7a/Wikipe-tan_full_length.svg/800px-Wikipe-tan_full_length.svg.png',
     'https://upload.wikimedia.org/wikipedia/commons/1/1f/Wiki-sisters.png'
   ]
-
-  useEffect(() => {
-    //change to props
-
-    const images = [
-      'https://upload.wikimedia.org/wikipedia/commons/thumb/7/7a/Wikipe-tan_full_length.svg/800px-Wikipe-tan_full_length.svg.png',
-      'https://upload.wikimedia.org/wikipedia/commons/1/1f/Wiki-sisters.png',
-      'https://upload.wikimedia.org/wikipedia/commons/thumb/7/7a/Wikipe-tan_full_length.svg/800px-Wikipe-tan_full_length.svg.png',
-      'https://upload.wikimedia.org/wikipedia/commons/thumb/7/7a/Wikipe-tan_full_length.svg/800px-Wikipe-tan_full_length.svg.png',
-      'https://upload.wikimedia.org/wikipedia/commons/1/1f/Wiki-sisters.png'
-    ]
-    setImagesHeight(Array(images.length).fill(0))
-
-    const layoutConfig = images.map((url, index) => {
-      const rowNumber = Math.floor(index / IMAGE_SELECT_COLUMNS)
-
-      return {
-        i: index.toString(),
-        x: index % IMAGE_SELECT_COLUMNS,
-        y: index >= IMAGE_SELECT_COLUMNS ? IMAGE_HEIGHT + rowNumber : 0,
-        w: 1,
-        h: Math.ceil(IMAGE_HEIGHT / IMAGE_SELECT_ROW_HEIGHT)
-      }
-    })
-
-    setLayout(layoutConfig)
-  }, [])
 
   return (
     <>
@@ -111,30 +78,7 @@ export function AddChapterModal({ showModal, setShowModal, refetchChapterList })
                     <div className={'m-2'}></div>
                     {FormCol('Id obrazu', 'number', 'imageId', 12, { min: 0 })}
                   </Row>
-                  <Row>
-                    {layout && (
-                      <GridLayout
-                        cols={IMAGE_SELECT_COLUMNS}
-                        className='layout'
-                        layout={layout}
-                        width={IMAGE_WIDTH * 2}
-                        rowHeight={IMAGE_SELECT_ROW_HEIGHT}
-                        isDraggable={false}
-                      >
-                        {images.map((url, index) => (
-                          <ImageContainer key={index.toString()}>
-                            <img
-                              className={'p-3'}
-                              width={'100%'}
-                              height={'100%'}
-                              src={url}
-                              alt={'info-task-attachment'}
-                            />
-                          </ImageContainer>
-                        ))}
-                      </GridLayout>
-                    )}
-                  </Row>
+                  <Row></Row>
                   <Row className='mt-4 d-flex justify-content-center'>
                     <Col sm={12} className='d-flex justify-content-center mb-2'>
                       <Button variant={'danger'} className={'me-2'} onClick={() => setShowModal(false)}>
@@ -157,6 +101,7 @@ export function AddChapterModal({ showModal, setShowModal, refetchChapterList })
                     </Col>
                   </Row>
                 </Container>
+                <BackgroundImagePicker width={700} cols={4} images={images} />
               </Form>
             )}
           </Formik>
---
+++ frontend/src/components/professor/GameManagement/AddChapterModal/BackgroundImagePicker.js
@@ -0,0 +1,69 @@
+import { useEffect, useState } from 'react'
+import GridLayout from 'react-grid-layout'
+import { ImageContainer } from '../../../general/ImagesGallery/ImagesGalleryStyle'
+
+export function BackgroundImagePicker(props) {
+  const [layout, setLayout] = useState(undefined)
+  const [imagesHeight, setImagesHeight] = useState([])
+  const [rowHeight, setRowHeight] = useState(undefined)
+
+  useEffect(() => {
+    setImagesHeight(Array(props.images.length).fill(0))
+
+    props.images.forEach((url, index) => {
+      const img = new Image()
+      img.src = url
+      img.onload = (e) => {
+        const scaledWidth = Math.floor(props.width / props.cols)
+        const scaledHeight = Math.floor((e.target.height * scaledWidth) / e.target.width)
+
+        setImagesHeight((prevState) => prevState.map((value, i) => (i === index ? scaledHeight : value)))
+      }
+    })
+  }, [props])
+
+  useEffect(() => {
+    if (!imagesHeight.includes(0)) {
+      setRowHeight(...imagesHeight)
+    }
+  }, [imagesHeight])
+
+  useEffect(() => {
+    if (rowHeight) {
+      const layoutConfig = props.images.map((url, index) => {
+        const rowNumber = props.cols > 0 ? Math.floor(index / props.cols) : 1
+
+        return {
+          i: index.toString(),
+          x: index % props.cols,
+          y: index >= props.cols ? imagesHeight[index - props.cols] + rowNumber : 0,
+          w: 1,
+          h: imagesHeight[index] > 0 && rowHeight > 0 ? Math.ceil(imagesHeight[index] / rowHeight) : 1
+        }
+      })
+
+      setLayout(layoutConfig)
+    }
+  }, [rowHeight, imagesHeight, props.images, props.cols])
+
+  if (!layout || !rowHeight) {
+    return null
+  } else {
+    return (
+      <GridLayout
+        cols={props.cols}
+        width={props.width}
+        className='layout'
+        layout={layout}
+        rowHeight={rowHeight > 0 ? rowHeight : props.width}
+        isDraggable={false}
+      >
+        {props.images.map((url, index) => (
+          <ImageContainer key={index.toString()}>
+            <img className={'p-3'} width={'100%'} height={'100%'} src={url} alt={'select-background'} />
+          </ImageContainer>
+        ))}
+      </GridLayout>
+    )
+  }
+}
