b6ffe8bc63159a94ba7acfb07de9d70e785fc312
kwdrt <jakubkcichocki@gmail.com>
2022-09-23 16:58:55 +0200

    missed cleanup

--- frontend/src/components/student/AllActivities/ExpeditionTask/QuestionSelectionDoor/QuestionSelectionDoor.js
+++ frontend/src/components/student/AllActivities/ExpeditionTask/QuestionSelectionDoor/QuestionSelectionDoor.js
@@ -1,17 +1,8 @@
-import React, { useEffect, useState } from 'react'
 import { Door, DoorColumn } from './QuestionSelectionDoorStyles'
 import { Button, Row } from 'react-bootstrap'
 import { Content } from '../../../../App/AppGeneralStyles'
-import { useLocation, useNavigate } from 'react-router-dom'
-import Loader from '../../../../general/Loader/Loader'
 import ExpeditionService from '../../../../../services/expedition.service'
 import { ERROR_OCCURRED } from '../../../../../utils/constants'
-import { GeneralRoutes, StudentRoutes } from '../../../../../routes/PageRoutes'
-
-// if only one element of id -1 or -2 then do not generate doors but go to score screen
-// will be done in routing after answering a question, so that we never get only the start or only the end node here
-
-//with API - with null?
 
 function generateDoor(question, expeditionId, noDoors, reloadInfo) {
   return (
@@ -47,40 +38,8 @@ function generateDoor(question, expeditionId, noDoors, reloadInfo) {
   )
 }
 
-// what do we need here?
-// activityId
-// reload callback
-// list of questions
-// remaining time
-
 function QuestionSelectionDoor(props) {
   const { activityId: expeditionId, questions, reloadInfo } = props
-
-  // useEffect(() => {
-  //   if (parentId == null || expeditionId == null || taskResultId == null) {
-  //     navigate(GeneralRoutes.HOME)
-  //   } else {
-  //     ExpeditionService.getChildQuestions(parentId)
-  //       .then((response) => setQuestions(response))
-  //       .catch(() => setQuestions(null))
-  //   }
-  // }, [parentId, expeditionId, navigate, taskResultId])
-
-  // useEffect(() => {
-  //   if (remainingTime === 0 || questionList?.length === 0) {
-  //     ExpeditionService.setSendTime(taskResultId, Date.now())
-  //       .then(() => {
-  //         navigate(StudentRoutes.GAME_MAP.GRAPH_TASK.SUMMARY, {
-  //           state: {
-  //             expeditionId: expeditionId,
-  //             remainingTime: remainingTime,
-  //             taskResultId: taskResultId
-  //           }
-  //         })
-  //       })
-  //       .catch(() => {})
-  //   }
-  // }, [questions, expeditionId, navigate, remainingTime, taskResultId])
 
   return (
     <Content>
