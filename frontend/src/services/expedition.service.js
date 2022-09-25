97a906ca4d1467913265fd630b57421c447b12ca
kwdrt <jakubkcichocki@gmail.com>
2022-09-25 15:43:55 +0200

    cleaned expeditionService from unused endpoint functions

--- frontend/src/services/expedition.service.js
+++ frontend/src/services/expedition.service.js
@@ -2,20 +2,13 @@ import { axiosApiGet, axiosApiPost } from '../utils/axios'
 import {
   GET_CURRENT_EXPEDITION_STATUS,
   GET_REMAINING_TIME,
-  GRAPH_GET_TASK_ANSWER_ID,
-  GRAPH_QUESTION,
-  GRAPH_QUESTION_NEXT,
   GRAPH_TASK_GET_ALL_POINTS,
   GRAPH_TASK_GET_CLOSED_POINTS,
-  GRAPH_TASK_GET_MAX_AVAILABLE_ALL,
   GRAPH_TASK_GET_MAX_AVAILABLE_CLOSED,
   GRAPH_TASK_GET_MAX_AVAILABLE_OPEN,
   GRAPH_TASK_RESULT_URL,
-  GRAPH_TASK_SEND_ANSWER,
   GRAPH_TASK_URL,
   SEND_EXPEDITION_ACTION,
-  SEND_EXPEDITION_FINISHED_TIME,
-  SET_START_TIME,
   START_EXPEDITION_TASK
 } from './urls'
 
@@ -30,26 +23,6 @@ class ExpeditionService {
   getExpeditionScore(activityId) {
     return axiosApiGet(GRAPH_TASK_RESULT_URL, {
       graphTaskId: activityId
-    }).catch((error) => {
-      throw error
-    })
-  }
-
-  saveAnswer(answer) {
-    return axiosApiPost(GRAPH_TASK_SEND_ANSWER, answer).catch((error) => {
-      throw error
-    })
-  }
-
-  getTaskAnswerId(activityId) {
-    return axiosApiPost(GRAPH_GET_TASK_ANSWER_ID, {
-      graphTaskId: activityId
-    }).catch(() => {})
-  }
-
-  getActivityMaxPoints(taskResultId) {
-    return axiosApiGet(GRAPH_TASK_GET_MAX_AVAILABLE_ALL, {
-      graphTaskResultId: taskResultId
     }).catch((error) => {
       throw error
     })
@@ -71,22 +44,6 @@ class ExpeditionService {
     })
   }
 
-  getQuestion(questionId) {
-    return axiosApiGet(GRAPH_QUESTION, {
-      questionId: questionId
-    }).catch((error) => {
-      throw error
-    })
-  }
-
-  getChildQuestions(parentQuestionId) {
-    return axiosApiGet(GRAPH_QUESTION_NEXT, {
-      questionId: parentQuestionId
-    }).catch((error) => {
-      throw error
-    })
-  }
-
   getExpeditionPointsMaxClosed(taskResultId) {
     return axiosApiGet(GRAPH_TASK_GET_MAX_AVAILABLE_CLOSED, {
       graphTaskResultId: taskResultId
@@ -103,27 +60,9 @@ class ExpeditionService {
     })
   }
 
-  setStartTime(resultId, timeMs) {
-    return axiosApiPost(SET_START_TIME, {
-      resultId: resultId,
-      startDateMillis: timeMs
-    }).catch((error) => {
-      throw error
-    })
-  }
-
   getRemainingTime(resultId) {
     return axiosApiGet(GET_REMAINING_TIME, {
       resultId: resultId
-    }).catch((error) => {
-      throw error
-    })
-  }
-
-  setSendTime(resultId, sendDateMillis) {
-    return axiosApiPost(SEND_EXPEDITION_FINISHED_TIME, {
-      resultId: resultId,
-      sendDateMillis: sendDateMillis
     }).catch((error) => {
       throw error
     })
