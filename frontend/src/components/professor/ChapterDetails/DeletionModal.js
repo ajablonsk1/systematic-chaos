cba4bea3fcbee97dbb3e69d8983aaeec51110ad2
Damian Cyper <damiancyper@gmail.com>
2022-10-02 15:57:26 +0200

    [SC-289] chapter deletion modal fixes

--- frontend/src/components/professor/ChapterDetails/DeletionModal.js
+++ frontend/src/components/professor/ChapterDetails/DeletionModal.js
@@ -1,24 +1,7 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { Button, Card, Modal } from 'react-bootstrap'
-import ChapterService from '../../../services/chapter.service'
-import { useNavigate } from 'react-router-dom'
-import { TeacherRoutes } from '../../../routes/PageRoutes'
-import { successToast } from '../../../utils/toasts'
 
 function DeletionModal(props) {
-  const navigate = useNavigate()
-  const [errorMessage, setErrorMessage] = useState('')
-
-  const deleteChapter = () => {
-    ChapterService.deleteChapter(props.chapterId)
-      .then(() => {
-        successToast('Rozdział usunięty pomyślnie.')
-        props.setModalOpen(false)
-        navigate(TeacherRoutes.GAME_MANAGEMENT.MAIN)
-      })
-      .catch((error) => setErrorMessage(error.response.data.message))
-  }
-
   return (
     <Modal show={props.showModal} onHide={() => props.setModalOpen(false)}>
       <Card>
@@ -30,10 +13,9 @@ function DeletionModal(props) {
           <Button className={'me-1'} variant={'secondary'} onClick={() => props.setModalOpen(false)}>
             Anuluj
           </Button>
-          <Button variant={'danger'} onClick={deleteChapter}>
+          <Button variant={'danger'} onClick={props.onClick}>
             Usuń
           </Button>
-          <p className={'text-danger'}>{errorMessage}</p>
         </Card.Footer>
       </Card>
     </Modal>
