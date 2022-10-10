c6b412bbae57591cd34d9cbb5522e49a196f80d5
Damian Cyper <damiancyper@gmail.com>
2022-10-10 17:45:49 +0200

    [SC-308] badge bug fix and activities to assessment counter displayed correctly

--- frontend/src/reducers/sidebar.js
+++ frontend/src/reducers/sidebar.js
@@ -1,12 +1,14 @@
-import { SET_EXPANDED } from '../actions/types'
+import { SET_ASSESSMENT_NUMBERS, SET_EXPANDED } from '../actions/types'
 
-const initialState = { isExpanded: true }
+const initialState = { isExpanded: true, assessments: 0 }
 
 export default function getSidebarInfo(state = initialState, action) {
   const { type, payload } = action
   switch (type) {
     case SET_EXPANDED:
       return { ...state, isExpanded: payload }
+    case SET_ASSESSMENT_NUMBERS:
+      return { ...state, assessments: payload }
     default:
       return state
   }
