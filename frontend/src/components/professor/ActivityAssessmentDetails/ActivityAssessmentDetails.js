51310475b5f876668f320a355e1d0999e6c6af23
Damian Cyper <63460537+dmncp@users.noreply.github.com>
2022-10-12 16:54:41 +0200

    [SC-308] Naprawienie bugów frontendowych (#230)
    * [SC-308] badge bug fix and activities to assessment counter displayed correctly
    
    * [SC-308] change buttons color
    
    * [SC-308] deps list update
    
    * [SC-308] game management colors change, add scrollbar for chapters list, new option for game settings view
    
    * [SC-308] display 0 for average file task result if backend returns null

--- frontend/src/components/professor/ActivityAssessmentDetails/ActivityAssessmentDetails.js
+++ frontend/src/components/professor/ActivityAssessmentDetails/ActivityAssessmentDetails.js
@@ -20,6 +20,7 @@ import { HorizontalSpacer, VerticalSpacer, Header } from '../../general/TaskShar
 import { ERROR_OCCURRED } from '../../../utils/constants'
 import { Activity } from '../../../utils/constants'
 import { connect } from 'react-redux'
+import { SET_ASSESSMENT_NUMBERS } from '../../../actions/types'
 
 function ActivityAssessmentDetails(props) {
   const navigate = useNavigate()
@@ -75,6 +76,7 @@ function ActivityAssessmentDetails(props) {
     } else {
       navigate(TeacherRoutes.ACTIVITY_ASSESSMENT.LIST)
     }
+    props.dispatch({ type: SET_ASSESSMENT_NUMBERS, payload: activityResponseInfo?.remaining ?? 0 })
   }
 
   useEffect(() => {
