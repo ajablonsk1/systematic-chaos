8fcb71c6ed9a4ca360c1029c056e10362e7ca71a
kwdrt <jakubkcichocki@gmail.com>
2022-10-16 13:03:09 +0200

    [SC-318] added getRemainingDate tests

--- frontend/src/tests/unit/Api.test.js
+++ frontend/src/tests/unit/Api.test.js
@@ -1,4 +1,4 @@
-import { parseJwt } from '../../utils/Api'
+import { getRemainingDate, parseJwt } from '../../utils/Api'
 import { testInvalidTokens, testProfessorTokens, testStudentTokens } from './testHelpers/tokens'
 
 describe('parseJwt() tests', () => {
@@ -22,5 +22,42 @@ describe('parseJwt() tests', () => {
 
     //then
     expect(parsedToken).toBeNull()
+  })
+})
+
+describe('getRemainingDate() tests', () => {
+  const endDates = [
+    {
+      date: '2022-05-14T12:00:00.000Z',
+      remainingDays: 1
+    },
+    {
+      date: '2022-05-13T12:00:00.000Z',
+      remainingDays: 0
+    },
+    {
+      date: '2022-05-12T12:00:00.000Z',
+      remainingDays: -1
+    },
+    {
+      date: '2022-07-15T13:50:00.000Z',
+      remainingDays: 63.076388888888886
+    },
+    {
+      date: '2022-05-13T12:00:00.001Z',
+      remainingDays: 1.1574074074074074e-8
+    }
+  ]
+
+  beforeAll(() => {
+    jest.useFakeTimers().setSystemTime(new Date('2022-05-13T12:00:00.000Z'))
+  })
+
+  it.each(endDates)('remaining time to date %s', (endDate) => {
+    //when
+    const remainingDate = getRemainingDate(endDate.date)
+
+    //then
+    expect(remainingDate.asDays()).toEqual(endDate.remainingDays)
   })
 })
