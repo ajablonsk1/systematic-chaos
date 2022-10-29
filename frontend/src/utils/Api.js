05b3f1d3ad1e220f168095cb8e31ef05d3198068
Jakub Cichocki <56168780+kwdrt@users.noreply.github.com>
2022-10-29 16:08:07 +0200

    [SC-318] Testy utils/Api.js (#240)
    * [SC-318] added first tests and delegated tokens to a helper
    
    * [SC-318] parseJwt tests finished
    
    * [SC-318] added getRemainingDate tests
    
    * [SC-318] added convertSecondsToStringInfo tests
    
    * [SC-318] changed folder name to storage
    
    * [SC-318] useRealTimers added
    
    * [SC-318] review fixes - convertDateToStringInfo
    
    * [SC-318] added undefined cases
    
    * [SC-318] added remaining cases for invalid objects
    
    * [SC-318] name update
    
    * [SC-318] missed a file
    
    * [SC-318] some review fixes
    
    * [SC-318] more review fixes - now checking isValid in Duration
    
    * [SC-318] returning moment.invalid() now
    
    * [SC-318] changes to tests
    
    * [SC-318] one small refactor

--- frontend/src/utils/Api.js
+++ frontend/src/utils/Api.js
@@ -1,5 +1,6 @@
 import moment from 'moment'
 import 'moment/locale/pl'
+import { INVALID_DATE_MESSAGE } from './constants'
 
 export const parseJwt = (token) => {
   try {
@@ -10,19 +11,27 @@ export const parseJwt = (token) => {
   }
 }
 
-export const getRemainingDate = (endDateString) => {
-  const today = moment(new Date())
-  const endDate = moment(new Date(endDateString))
-
-  // duration
-  return moment.duration(endDate.diff(today))
+export const getRemainingDate = (endDateGiven) => {
+  if (typeof endDateGiven === 'number') {
+    // duration
+    const today = moment(new Date())
+    const endDate = moment(new Date(endDateGiven))
+    return moment.duration(endDate.diff(today))
+  }
+  return INVALID_DATE_MESSAGE
 }
 
-export const convertSecondsToStringInfo = (endDate) => {
+// we should ALWAYS get date as millis from back from what there currently is
+// also we need to update the fetch as it relies on old api and will always show '-'
+export const convertDateToStringInfo = (endDate) => {
   const duration = getRemainingDate(endDate)
-  const days = Math.floor(duration.asDays())
-  const stringFormat = moment.utc(duration.asSeconds() * 1000).format('HH:mm:ss')
-  const splitString = stringFormat.split(':')
+  if (duration !== INVALID_DATE_MESSAGE) {
+    const days = Math.floor(duration.asDays())
+    const stringFormat = moment.utc(duration.asSeconds() * 1000).format('HH:mm:ss')
+    const splitString = stringFormat.split(':')
 
-  return `${days} dni, ${splitString[0]} godz, ${splitString[1]} min`
+    return `${days} dni, ${splitString[0]} godz, ${splitString[1]} min`
+  }
+
+  return INVALID_DATE_MESSAGE
 }
