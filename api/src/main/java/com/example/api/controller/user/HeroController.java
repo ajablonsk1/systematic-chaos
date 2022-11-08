fbcdc820e10ca8d452c509768ed2a06e136446f2
ajablonsk1 <adrian.tomasz.jablonski@gmail.com>
2022-11-08 13:54:31 +0100

    [SC-340] Changed @PostMapping to @PutMapping

--- api/src/main/java/com/example/api/controller/user/HeroController.java
+++ api/src/main/java/com/example/api/controller/user/HeroController.java
@@ -6,10 +6,7 @@ import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -18,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HeroController {
     private final HeroService heroService;
 
-    @PostMapping
+    @PutMapping
     public ResponseEntity<?> updateHero(@RequestBody UpdateHeroForm form) {
         heroService.updateHero(form);
         return new ResponseEntity<>(HttpStatus.OK);
